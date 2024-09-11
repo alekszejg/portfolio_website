73233ed93ea180c989c3740f87ecb13b836115d3
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-11 02:54:01 +0200

    Builds individual blogposts and converts default UTC time to local time

---
+++ Components/Reusable/blogpost.tsx
@@ -0,0 +1,36 @@
+import { PostType } from "@/actions/blog/getBlogposts";
+
+export default function Blogpost(props: PostType) {
+    const {id, title, content, category_ID, created_at } = props;
+
+    const createdAtLocal = new Date(created_at.toLocaleString());
+    const dateFormat = new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false // no AM or PM
+    });
+
+    const createdAtFormatted = dateFormat.format(createdAtLocal);
+
+    return (
+        <div className="blogpost">
+            <div className="headerWrapper">
+                <div className="profileWrapper">
+                    <div className="profileImgWrapper">
+                        <img src="/Images/myPhoto.png" alt="My Photo" />
+                    </div>
+                    <h3>Alekszej Guljajev</h3>
+                </div>
+                <p className="createdAtInfo">at {createdAtFormatted}</p>
+                
+            </div>
+            <h4>{title}</h4>
+        
+            <p className="content">{content}</p>
+            
+        </div>
+    );
+}
