3228d23f20ad89b8a9ac3ffecedb91342419439a
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-17 18:50:42 +0200

    Added api route to fetch individual blogposts by ID. Possibly rename it in future

---
+++ app/api/blogposts/post-data/route.ts
@@ -0,0 +1,24 @@
+import { pool } from "@/postgres";
+import type { PoolClient } from "pg";
+import { NextRequest, NextResponse } from 'next/server';
+import type { PostType } from "@/actions/blog/getRecentPosts";
+
+export async function GET(req: NextRequest) {
+    const searchParams = req.nextUrl.searchParams
+    const postID = searchParams.get('postID')
+    
+    let res: PostType | null = null;
+    let client: PoolClient | null = null;
+
+    try {
+        client = await pool.connect();
+        const response = await client.query('SELECT * FROM posts WHERE id = $1', [postID]);
+        res = response.rows[0];
+        return NextResponse.json({postData: res}, { status: 200 });
+    } catch (error: any) {
+        console.error('Error executing query', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    } finally {
+        client && client.release();
+    }
+}
