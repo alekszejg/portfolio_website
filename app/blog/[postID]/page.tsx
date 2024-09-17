de4f2fe2b1f0efb5614520c7144bf0f40cdec201
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-17 18:46:44 +0200

    Now both generatestaticparams and blogpostpage use api routes instead of server actions

--- app/blog/[postID]/page.tsx
+++ app/blog/[postID]/page.tsx
@@ -1,15 +1,23 @@
-import fetchAllPostID from "@/actions/blog/fetchAllPostID";
-import fetchPostByID from "@/actions/blog/fetchPostByID";
 import Blogpost from "../blogpost";
 
 export async function generateStaticParams() {
-    const ids = await fetchAllPostID();
-    return ids;
+    const response = await fetch('http://localhost:3000/api/blogposts/get-all-post-ids');
+    
+    if (response.ok) {
+        const data = await response.json();
+        return data.post_ids;
+    } else {
+        console.error("Failed to fetch total posts", response.statusText);
+    }
 }
 
 
 export default async function BlogpostPage({params}: {params: {postID: string}}) {
-    const postData = await fetchPostByID(params.postID);
-    if (postData) return <Blogpost {...postData} />;
-    else return null;
+    console.log("parameter is ", params.postID)
+    const response = await fetch(`http://localhost:3000/api/blogposts/post-data?postID=${params.postID}`);
+    
+    if (response.ok) {
+        const data = await response.json();
+        return <Blogpost {...data.postData} />
+    } else return null;
 }
