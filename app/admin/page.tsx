0a1e066e9b6bec6ca7f4ada22518828ee4b47445
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-17 18:42:22 +0200

    now preloads recent posts from API route

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,33 +2,15 @@ import PageLayout from "@/Components/Layout/pageLayout";
 import PostCreator from "./postCreator";
 import RecentPosts from "./recentPosts";
 import "@/Styling/Pages/adminPage.scss";
-import { PoolClient } from "pg";
-import { pool } from "@/postgres";
-import getRecentPosts, { PostType} from "@/actions/blog/getRecentPosts";
-
 import ChoosePostCategory from "./choosePostCategory";
-
 import DisplayTotalPosts from "./displayTotalPosts";
 
 export default async function AdminPage() {
-    let client: PoolClient | null = null;
-    let recentPosts: PostType[] = []; 
-
-    
-    try {
-        client = await pool.connect();
-        recentPosts = await getRecentPosts({client: client, offset: 0});
-    } catch (error: any) {
-        console.error("error: ", error);
-    } finally {
-        client && client.release();
-    }
-    
 
     return (
         <PageLayout layoutID="adminPageLayout">
             <PostCreator selectCategory={ChoosePostCategory()} />
-            <RecentPosts poolClient={client} recentPosts={recentPosts} />
+            <RecentPosts />
             <div>
                 Testing total posta api<br/><br/>
                 <DisplayTotalPosts />
