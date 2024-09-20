91b592ed75d618bb778e37805e84f574f8777af4
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-20 05:06:18 +0200

    Placed social media styled part of code into separate component to keep homepage simple. Ideally all pages should only contain components and general structure, not separate elements

---
+++ app/_homepage/socialProfile.tsx
@@ -0,0 +1,40 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot, faLink } from '@fortawesome/free-solid-svg-icons';
+
+
+export default function SocialProfile() {
+    return (
+        <>
+        <div id="bgPhotoWrapper">
+            <img src="/Images/myLogo.svg" alt="My logo in background" />
+        </div>
+        <div id="myPhotoWrapper">
+            <img src="/Images/myPhoto.png" alt="My Photo" />
+        </div>
+        
+        <div id="textWrapper">
+            <h2>Alekszej Guljajev</h2>
+            <p>A self-taught programmer with BSc Economics Diploma who has been programming for a year in
+                both Python and JavaScript. My portfolio may be no so large yet, but I guarantee that I invest all my 
+                soul, passion and energy into projects I make. Currently aiming to get a job in the industry, meanwhile
+                working on new projects and improving existing ones.  
+            </p>
+            
+            <p id="location">
+                <FontAwesomeIcon id="locationIcon" icon={faLocationDot} />
+                Munich, Germany
+            </p>
+            
+            <div className="visitLinkWrapper">
+                <FontAwesomeIcon className="linkIcon" icon={faLink} />
+                <a href="https://github.com/alekszejg" target="_blank" rel="noopener noreferrer">Github</a>
+            </div>
+
+            <div className="visitLinkWrapper">
+                <FontAwesomeIcon className="linkIcon" icon={faLink} />
+                <a href="https://tryhackme.com/p/alekszejg" target="_blank" rel="noopener noreferrer">TryHackMe</a>
+            </div>
+        </div>
+        </>  
+    );
+}
