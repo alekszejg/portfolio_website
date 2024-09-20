d08538754cc752489be3c60e69f97f8d24d862a9
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-20 05:05:09 +0200

    Split recent posts into show recent posts component which does fetch request and recent posts section of homepage which displays those recent posts (possibly reusable)

---
+++ app/_homepage/recentPosts.tsx
@@ -0,0 +1,10 @@
+import ShowRecentPosts from "../admin/showRecentPosts";
+
+export default function RecentPosts() {
+    return (
+        <section id="recentPostsSection">
+            <h2>Recent posts</h2>
+            <ShowRecentPosts />
+        </section>
+    );
+}
--- app/admin/showRecentPosts.tsx
+++ app/admin/showRecentPosts.tsx
@@ -2,14 +2,18 @@ import Blogpost from "@/app/blog/blogpost";
 import type { PostType } from "../api/blogposts/get-recent-posts/route";
 
 
-export default async function RecentPosts() {
+export default async function ShowRecentPosts() {
     const response = await fetch('http://localhost:3000/api/blogposts/get-recent-posts?offset=0');
     
     if (response.ok) {
         const data = await response.json();
         const posts: PostType[] = data.recent_posts;
 
-        return <>{posts.map(post => (<Blogpost key={post.id} {...post} />))}</>;
+        return (
+            <div id="recentPostsWrapper">
+                {posts.map(post => (<Blogpost key={post.id} {...post} />))}
+            </div>
+        );
     } 
     else return null;
 }
