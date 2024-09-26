7c38fdce3457750cbbc6165cef90a09d85159056
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-26 18:24:05 +0200

    provider is now on separate options file which allows to use getServerSSession and handler is inside route.ts file for authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,56 +1,5 @@
 import nextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import type { User } from "next-auth";
+import { authOptions } from "./options";
 
-const handler = nextAuth({
-    providers: [
-      CredentialsProvider({
-        credentials: {
-          username: {label: "username", type: "text"},
-          password: {label: "password", type: "password"},
-        },
-        
-        async authorize(credentials:  Record<"username" | "password", string> | undefined): Promise<User | null> {
-          if (!credentials) {
-            return null;
-          }
-          
-          const { username, password } = credentials;
-      
-          if (!(username === process.env.admin_user && password === process.env.admin_password)) {
-            return null
-          } else {
-            return {id: "1", username: username};
-          }
-
-        }
-      })
-    ],
-
-    session: {
-      strategy: 'jwt'
-    },
-
-    pages: {
-      signIn: '/admin/sign-in'
-    },
-
-    callbacks: {
-      async session({session, token}) {
-        if (token) {
-          session.id = token.id;
-          session.username = token.username;
-        }
-        return session;
-      },
-      async jwt({token, user}) {
-        if (user) {
-          token.id = user.id;
-          token.username = user.username;
-        }
-        return token;
-      }
-    }
-});
-
-export {handler as GET, handler as POST};
+const handler = nextAuth(authOptions);
+export { handler as GET, handler as POST };
