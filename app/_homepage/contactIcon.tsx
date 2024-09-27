974d527eb0eedcabade49851aad9f03796107878
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-27 16:30:24 +0200

    Added feedback from message submission. Error text must still be tested and styled better

--- app/_homepage/contactIcon.tsx
+++ app/_homepage/contactIcon.tsx
@@ -1,8 +1,8 @@
 "use client"
-import { useState, useRef } from 'react';
+import { useState, useRef, FormEvent } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-
+import handleMessageSubmit from '@/actions/handleMessageSubmit';
 
 export default function ContactIcon() {
     const dialogRef = useRef<HTMLDialogElement>(null);
@@ -17,17 +17,37 @@ export default function ContactIcon() {
             dialogRef.current?.showModal()
         }
     }
+
+    const [ status, setStatus ] = useState({nameError: "", emailError: "", messageError: "", submitted: false}); 
+    const { nameError, emailError, messageError, submitted } = status;
+
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+        const { nameError, emailError, messageError, submitted } = await handleMessageSubmit(new FormData(event.currentTarget as HTMLFormElement));
+        setStatus({nameError: nameError, emailError: emailError, messageError: messageError, submitted: submitted})
+        if (submitted) {
+            setPopupVisibility(false);
+            setTimeout(() => {dialogRef.current?.close();}, 2000);
+        }
+    }
   
     return (
         <>
         <FontAwesomeIcon id="mailIcon" icon={faEnvelope} onClick={toggleDialogie} />
         <dialog id="contactPopup" ref={dialogRef}>
             <button id="closeButton" onClick={toggleDialogie}><img src="/Icons/closeIcon.svg" alt="close popup" /></button>
-            <form>
+            <form onSubmit={handleSubmit}>
+                {nameError && <p>{nameError}</p>}
                 <input type="text" name="name" placeholder="Name" />
-                <input type="text" name="contactInfo" placeholder="Email or phone number" />
-                <textarea name="message" placeholder="How can I help you?" />
-                <button id="sendButton"><img src="/Icons/sendMessage.svg" alt="send message" /></button>
+                
+                {emailError && <p>{emailError}</p>}
+                <input type="text" name="contactEmail" placeholder="Email (optional)" />
+                
+                {messageError && <p>{messageError}</p>}
+                <textarea name="message" placeholder="How can I help you?" required />
+                
+                <button id="sendButton" type="submit"><img src="/Icons/sendMessage.svg" alt="send message" /></button>
+                {submitted && <p id="submittedText">Message has been submitted</p>}
             </form>
         </dialog>
         </>
