fdb63c7042188018e9beb1c4cd20fe2035731879
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-29 17:22:45 +0200

    Fixed hydration error where images were missing first '/' symbol

--- app/_homepage/myStack.tsx
+++ app/_homepage/myStack.tsx
@@ -29,7 +29,7 @@ export default function MyStack(props: {hasHeader: boolean}) {
             text: `As it is the foundation of web development I am quite confident with it, but I still
             learn new information and tricks when making projects, which wouldn't be otherwise obtained through reading
             theory and tutorials.`, 
-            image: "Images/Logos/html.svg",
+            image: "/Images/Logos/html.svg",
             alt: "HTML clickable Logo"
         },
         "css": {
@@ -37,7 +37,7 @@ export default function MyStack(props: {hasHeader: boolean}) {
             text: `As it is the foundation of web development I am quite confident with it, but I still
             learn new information and tricks when making projects, which wouldn't be otherwise obtained through reading
             theory and tutorials.`, 
-            image: "Images/Logos/css.svg",
+            image: "/Images/Logos/css.svg",
             alt: "CSS clickable Logo"
         },
         "sass/scss": {
@@ -46,7 +46,7 @@ export default function MyStack(props: {hasHeader: boolean}) {
             with many components, working with SCSS allows me to practice and improve my CSS, while achieving smaller stylesheets
             with simple-syntaxed variables, mixins and nesting, which leads to significant improvements in code readability
             and maintability.`, 
-            image: "Images/Logos/sass.svg",
+            image: "/Images/Logos/sass.svg",
             alt: "Sass / SCSS clickable Logo"
         },
         "tailwind-css": {
@@ -57,7 +57,7 @@ export default function MyStack(props: {hasHeader: boolean}) {
             While it would make HTML or JavaScript content larger and less readable, I found out that keeping all styles in organized
             and categorized dictionary allows more control of children components through parent and offers quick overview of all
             applied styles.`, 
-            image: "Images/Logos/tailwind.svg",
+            image: "/Images/Logos/tailwind.svg",
             alt: "Tailwind CSS clickable logo"
         },
 
@@ -69,7 +69,7 @@ export default function MyStack(props: {hasHeader: boolean}) {
             of UI and services offered by other people, if frontend is editable. As an example, I used JavaScript to 
             customize default behaviour and styling of AddEvent.com calendars and events of AddEvent.com for Mangopost 
             company as well as edit stickers they have printed from Backend data.`, 
-            image: "Images/Logos/js.svg",
+            image: "/Images/Logos/js.svg",
             alt: "JavaScript clickable logo"
         },
         "typescript": {
@@ -79,7 +79,7 @@ export default function MyStack(props: {hasHeader: boolean}) {
             to spot the issues. While I'm enjoying the lack of unexpected type and syntax errors during the development 
             along with great features like optional object's properties, simple typeguards and clearly defined function parameters,
             I aim to keep learning and using more of its advanced features to make the types more reusable and my code more readable.`,
-            image: "Images/Logos/ts.svg",
+            image: "/Images/Logos/ts.svg",
             alt: "TypeScript clickable logo"
         },
         "python": {
