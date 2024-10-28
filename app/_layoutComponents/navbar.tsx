a31ee0ff6d2ec613e1c77edcfb721d1711d231b7
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-28 23:13:06 +0100

    Navbar is now styled with Tailwind instead of SCSS. Removed its .scss file

--- app/_layoutComponents/navbar.tsx
+++ app/_layoutComponents/navbar.tsx
@@ -1,25 +1,20 @@
 "use client"
+import Image from 'next/image';
 import { useState, useEffect, useRef } from 'react';
 import Link from "next/link";
-import '@/app/_layoutComponents/Styling/navbar.scss'
-
+import urlPaths from "@/urlPaths.json";
 
 export default function Navbar() {
     const [burgerMenuOpened, setBurgerMenu] = useState(false); 
-    
     const toggleBurgerMenu = () => setBurgerMenu(!burgerMenuOpened);
    
     const burgerIconRef = useRef<HTMLImageElement>(null);
     const mobileMenuRef = useRef<HTMLUListElement>(null);
     
     useEffect(() => {
-        if (burgerMenuOpened) {
-            document.body.classList.add("disableScroll");
-        } else {
-            document.body.classList.remove("disableScroll");
-        }
+        const bodyClasses = document.body.classList;
+        burgerMenuOpened ? bodyClasses.add("disableBodyScroll") : bodyClasses.remove("disableBodyScroll")
     }, [burgerMenuOpened]);
-
 
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
@@ -32,26 +27,47 @@ export default function Navbar() {
         return () => document.removeEventListener('mousedown', handleClickOutside);
     }, [])
 
-    const links = [
-        {title: "Interactive CV", href: "/interactive-cv"},
-        {title: "Projects", href: "/projects"},
-    ];
+    const styling = {
+        nav: "flex justify-between items-center max-w-[1400px] h-[3rem] relative bg-[white] ultrawide:mx-auto",
+        burgerMenuButton: "h-[1.6rem] p-0 mx-[0.6rem] bg-transparent border-0 tablet:hidden",
+        burgerMenuImg: "w-full h-full",
+        navListMobile: burgerMenuOpened ? "flex flex-col w-3/5 h-screen p-0 m-0 absolute top-full bg-[rgba(190,190,190,0.95)]" : "hidden",
+        navListPC: "tablet:flex tablet:flex-row tablet:w-[30%] tablet:h-full tablet:ml-[1rem] tablet:static tablet:bg-inherit",
+        navListItem: "flex justify-center items-center w-full h-[3rem] hover:bg-[rgba(150,150,150,0.7)] tablet-h-full",
+        navLink: "flex justify-center items-center w-full h-full text-white font-medium tracking-wider tablet:inline tablet:h-auto tablet:text-center tablet:text-nowrap tablet:text-black tablet:text-[1.1rem] tablet:opacity-50",
+        logoLink: "w-2/5 h-full tablet:w-[min(150px,20%)]",
+        logoImg: "w-full h-full"
+    }
 
     return (
-        <nav> 
-            <button id="burgerMenuButton">
-                <img ref={burgerIconRef} src="/Icons/burgerMenuIcon.svg" alt="toggle mobile menu" onClick={toggleBurgerMenu} />
+        <nav className={styling.nav}> 
+            <button className={styling.burgerMenuButton}>
+                <Image 
+                ref={burgerIconRef}
+                className={styling.burgerMenuImg}
+                width={0}
+                height={0} 
+                src="/Icons/burgerMenuIcon.svg" 
+                alt="toggle mobile menu" 
+                onClick={toggleBurgerMenu} />
             </button>
             
-            <ul className={burgerMenuOpened ? "navList burgerMenuOpened" : "navList burgerMenuClosed"} ref={mobileMenuRef}>
-                {links.map((link, index) => (
-                    <li key={`linkWrapper${index + 1}`}>
-                        <Link href={link.href}>{link.title}</Link>
+            <ul className={`${styling.navListMobile} ${styling.navListPC}`}>
+                {urlPaths.map((link, index) => (
+                    <li key={`linkWrapper${index + 1}`} className={styling.navListItem}>
+                        <Link href={link.href} className={styling.navLink}>{link.title}</Link>
                     </li>
                 ))}
             </ul>
         
-            <Link id="logoLink" href="/"><img src="" alt="logo" /></Link>
+            <Link className={styling.logoLink} href="/">
+                <Image 
+                    className={styling.logoImg} 
+                    width={0} 
+                    height={0} 
+                    src="" 
+                    alt="logo" />
+            </Link>
         </nav>
     );
 }
