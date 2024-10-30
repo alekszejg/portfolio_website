d54595e38dbe068d07ec36e4bfdb63ec597b67ac
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-30 19:43:59 +0100

    Added a separate file to store large object with strings for CV page. Decided to go for .ts extension instead of JSON for readability + ability to add types later. Do the same for homepage alter, move information from JSON files into .ts

---
+++ app/interactive-cv/cv.info.ts
@@ -0,0 +1,83 @@
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
+
+
+const cvInfo = {
+    contactMe: [
+        {id: "contactEmail", icon: faEnvelope, text: "alexeyguljajev@gmail.com"},
+        {id: "contactPhone", icon: faPhone, text: "+49 174 6541424"},
+    ],
+    education: {
+        "School N25 in Ussuriysk, Russia": "2007 - 2012",
+        "Munich International School in Munich, Germany": "2013 - 2020",
+        "University of Westminster in London, UK": "2020 - 2023",
+        "Self-taught programer": "2023 -"
+    },
+    programmingApproaches: {
+        "Most tasks are programmable": `Most tasks that involve computers, specifically working with data and its manipulation, 
+        can be programmed. While due to API limitations or calculation complexit the solution found might not be ideal, 
+        or be different to previous expectations,there will be at least a single solution for the problem. Keeping this 
+        thought in mind allows me to relax and face challenges with confidence, focusing on brainstorming and testing 
+        potential solutions, rather than question feasibility of faced challenge.`,
+    
+        "Experiment with available technology and tools": `Not learning or using available technology would be a missed
+        opportunity, since various tools, packages and modules can singificantly optimize mundane programming tasks. Even
+        using most basic well-known features provides me more knowledge and experience with that technology compared 
+        to those who never used it.`,
+        
+        "Understand concepts on fundamental in-depth level": `While developers have access to many tools such
+        as website constructors, modules and packages that abstract and simplify various programming processes, 
+        it's still important to me to look for underlying concepts and patterns these tools have. Gaining more knowledge
+        about fundamental concepts and processes allows me to ask more questions which lead to finding more detailed 
+        answers and connecting recently acquired information with knowledge that I already have.`,
+        
+        "Assume all clients have no programming knowledge": `During the development of a Discord bot I was investigating 
+        possible methods to enhance interaction between users and the bot. It has often led me to forums where I encountered
+        hobbyists who provided very detailed instructions on how to use their bot and specific input each command expects to 
+        be executed successfully. I find such approach to be confusing and repelling to the end-user. I believe that we as
+        developers must simplify all user interactions with our program and anticipate all probable and improbable edge 
+        cases that may occur during the interaction. We shouldn't just rely on user to behave exactly as we expect.`,
+    
+        "Start simple, then gradually add complexity when needed": `Everything in a program needs to be as simple and intutive 
+        as possible. This applies to the code our applicaiton was programmed in, but it's especially crucial for 
+        UI, which guides users through the application. For example using universally recognizable symbols for 
+        specific buttons and functionalities would be more effective than re-inventing the wheel by re-designing those 
+        symbols or creating new icons that represent them. In my opinion, the intuitiveness and simplicity of UI comes 
+        from user's prevous experiences, habits and even muscle memory. If we can develop an application or UI that 
+        resonates with users, there is a high chance that our application and UI have achieved their goals.`,
+        
+        "Ensure the code is clear and easily readable": `While many of us chase the satisfying, perfectly functioning 
+        end-result, it's crucial to look back and ensure our code can be easily read and understood by others, as well as
+        by our future selves. Standards for clear code might be subjective, but adding some indentation, line-breaks and 
+        comments is the minimum I can do. When possible, I combine similar code into reusable functions and components or 
+        split large chunk of code into separate files. I try to avoid writing long lines of code or having files that 
+        exceed 300 lines of code to ensure that my code is clear and easy to navigate, at least for myself in the future`,
+    
+        "Take notes when possible": `Taking notes plays a huge role in my learning and the way I process new information.
+        While I might not return to my notes immediately, writing down new information gives me additional time to 
+        process and understand it better. It also helps me to refresh some long-forgotten concepts and ideas and it helps
+        to avoid searching the internet for informaiton that I have already looked up. During my projects, I sometimes 
+        encounter rarely mentioned information and observations, which I can then re-read when facing a similar problem 
+        again.`,
+    },
+    technicalSkills: [
+        "HTML CSS", 
+        "SASS / SCSS", 
+        "Tailwind CSS", 
+        "JavaScript & TypeScript", 
+        "React (& migration to Next.js 14)", 
+        "Python", 
+        "Adobe Illustrator", 
+        "Figma", 
+        "Research"
+    ],
+    softSkills: [
+        "Punctuality", 
+        "Communication", 
+        "Ready to help", 
+        "Patience"
+    ]
+}
+
+export default cvInfo;
+   
