20ad9cf95d4d7a3e4a8b6034eed005c898e9c4b4
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-30 17:33:13 +0100

    Added tailwind mobile styling instead of scss

--- app/_homepage/project.tsx
+++ app/_homepage/project.tsx
@@ -1,23 +1,20 @@
 "use client"
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-type Project = {
-    githubUrl: string, 
-    localUrl: string
-    imgSrc: string,
-    imgAlt: string, 
-    title: string
-};
+
+type Project = {githubUrl: string, localUrl: string, imgSrc: string, imgAlt: string, title: string};
+
 
 export default function Project(props: Project) {
     const { githubUrl, localUrl, imgSrc, imgAlt, title } = props;
 
-    const [buttonMenuVisible, setButtonMenuVisibility] = useState(false);
+    const [buttonsVisible, setButtonsVisibility] = useState(false);
     const toggleButtonMenu = (event: React.MouseEvent<HTMLElement>) => {
-        event.preventDefault(); // stops <Link> parent from being clicked
-        setButtonMenuVisibility(prev => !prev);
+        event.preventDefault(); 
+        setButtonsVisibility(prev => !prev);
     };
 
     const router = useRouter();
@@ -25,24 +22,48 @@ export default function Project(props: Project) {
         event.preventDefault();
         router.push(localUrl);
     };
+
+    const styling = {
+        link: "flex flex-col items-center w-full text-black",
+        projectImgWrapper: "w-full aspect-[4/5] border-1 border-[black] relative",
+        projectImgRegular: "w-full h-full",
+        projectImgTransparent: "w-full h-full opacity-20",
+        projectName: "mt-2 text-center font-medium",
+        buttons: {
+            menu: "flex flex-col justify-center items-center gap-x-[0.8rem] absolute top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%]",
+            button: "w-full py-2 px-4 text-nowrap transition duration-100 ease-in-out",
+            toggleMenuButton: "w-6 absolute top-2 right-2",
+            toggleMenuImg: "w-full h-full",
+        }
+    }
     
     return (
         <Link href={localUrl} className="projectWrapperLink">
             
-            <div className="projectImgWrapper">
-                <img src={imgSrc} alt={imgAlt} className={buttonMenuVisible ? "semiTransparentImg" : undefined} />
+            <div className={styling.projectImgWrapper}>
+                <Image 
+                className={buttonsVisible ? styling.projectImgTransparent : styling.projectImgRegular} 
+                width={1000}
+                height={1000}
+                src={imgSrc} 
+                alt={imgAlt}  />
 
-                <div className={buttonMenuVisible ? "buttonMenu" : "buttonMenuClosed"}>
-                    <button className="toGithubButton" onClick={() => window.open(`${githubUrl}`, '_blank')}>Github</button>
-                    <button className="moreInfoButton" onClick={handleReadMore}>More info</button>
+                <div className={buttonsVisible ? styling.buttons.menu : "hidden"}>
+                    <button className={styling.buttons.button} onClick={() => window.open(`${githubUrl}`, '_blank')}>Github</button>
+                    <button className={styling.buttons.button} onClick={handleReadMore}>More info</button>
                 </div>
 
-                <div className="showButtonMenuIconWrapper" onClick={toggleButtonMenu}>
-                    <img src="/Icons/infoIcon.svg" alt="show information" />
-                </div>
+                <button className={styling.buttons.toggleMenuButton} onClick={toggleButtonMenu}>
+                    <Image 
+                    className={styling.buttons.toggleMenuImg} 
+                    width={100}
+                    height={100}
+                    src="/Icons/infoIcon.svg" 
+                    alt="show information" />
+                </button>
             </div>
 
-            <h3>{title}</h3>
+            <h3 className={styling.projectName}>{title}</h3>
         </Link>
     );
 }
