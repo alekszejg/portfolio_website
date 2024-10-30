3fb070214f60e50979ea9ae41333ff9b5ba42e81
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-30 17:33:41 +0100

    added tailwind mobile styling instead of scss

--- app/_homepage/sections/myProjects.tsx
+++ app/_homepage/sections/myProjects.tsx
@@ -1,67 +1,19 @@
 import Project from "@/app/_homepage/project";
-
+import projects from "@/app/_homepage/myProjects.json";
 
 export default function MyProjects(props: {hasHeader: boolean}) {
-    
-    const discordBotProps = {
-        githubUrl: "https://github.com/alekszejg/discord_bot_python",
-        localUrl: "/projects/discord-bot",
-        imgSrc: "/Images/Project Previews/discordBot.svg", 
-        imgAlt: "discord bot icon",
-        title: "Discord Bot"
-    };
-
-    const portfolioWebsiteProps = {
-        githubUrl: "https://github.com/alekszejg/portfolio_website",
-        localUrl: "/projects/portfolio-website",
-        imgSrc: "/Images/Project Previews/thisWebsite.png", 
-        imgAlt: "this website's homepage",
-        title: "Portfolio Website"
-    }
-
-    const mangopostWebsiteProps = {
-        githubUrl: "https://github.com/alekszejg/mangopost_nextjs",
-        localUrl: "/projects/mangopost-website",
-        imgSrc: "/Images/Project Previews/mangopostWebsite.png", 
-        imgAlt: "Mangopost website screenshot",
-        title: "Mangopost Website"
-    }
-
-    const magopostWebsiteProps = {
-        githubUrl: "https://github.com/alekszejg/mangopost_nextjs",
-        localUrl: "/projects/mangopost-website",
-        imgSrc: "/Images/Project Previews/mangopostWebsite.png", 
-        imgAlt: "Mangopost website screenshot",
-        title: "Mangopost Website"
-    }
-
-    const mngopostWebsiteProps = {
-        githubUrl: "https://github.com/alekszejg/mangopost_nextjs",
-        localUrl: "/projects/mangopost-website",
-        imgSrc: "/Images/Project Previews/mangopostWebsite.png", 
-        imgAlt: "Mangopost website screenshot",
-        title: "Mangopost Website"
-    }
-
-    const mangopotWebsiteProps = {
-        githubUrl: "https://github.com/alekszejg/mangopost_nextjs",
-        localUrl: "/projects/mangopost-website",
-        imgSrc: "/Images/Project Previews/mangopostWebsite.png", 
-        imgAlt: "Mangopost website screenshot",
-        title: "Mangopost Website"
+    const styling = {
+        section: "py-[3rem] px-[5%]",
+        projectsGrid: "grid grid-cols-[repeat(2, 1fr)] gap-[2rem]"
     }
 
     return (
-        <section id="projectsSection">
+        <section className={styling.section}>
             {props.hasHeader && <h2>Projects</h2>}
-            <div id="projectsGrid">
-                <Project  {...discordBotProps} />
-                <Project {...portfolioWebsiteProps} />
-                <Project {...mangopostWebsiteProps} />
-
-                <Project {...magopostWebsiteProps} />
-                <Project {...mngopostWebsiteProps} />
-                <Project {...mangopotWebsiteProps} />
+            <div className={styling.projectsGrid}>
+                <Project  {...projects.discordBot} />
+                <Project {...projects.portfolioWebsite} />
+                <Project {...projects.mangopostWebsite} />
             </div>
         </section>
     );
