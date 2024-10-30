3d5e50a058c1c1c658b2f844e750c64e6ded2d3b
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-30 18:30:08 +0100

    Added desktop tailwind styling along with group hover effect on projects

--- app/_homepage/project.tsx
+++ app/_homepage/project.tsx
@@ -25,14 +25,15 @@ export default function Project(props: Project) {
 
     const styling = {
         link: "flex flex-col items-center w-full text-black",
-        projectImgWrapper: "w-full aspect-[4/5] border-1 border-[black] relative",
-        projectImgRegular: "w-full h-full",
+        projectImgWrapper: "w-full aspect-[4/5] border-1 border-[black] relative group",
+        projectImgRegular: "w-full h-full tablet:group-hover:opacity-40",
         projectImgTransparent: "w-full h-full opacity-20",
-        projectName: "mt-2 text-center font-medium",
+        projectName: "mt-2 text-center font-medium tablet:hover:scale-105 tablet:hover:opacity-60",
         buttons: {
-            menu: "flex flex-col justify-center items-center gap-x-[0.8rem] absolute top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%]",
-            button: "w-full py-2 px-4 text-nowrap transition duration-100 ease-in-out",
-            toggleMenuButton: "w-6 absolute top-2 right-2",
+            menu: "flex flex-col justify-center items-center gap-x-[0.8rem] absolute top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] tablet:group-hover:flex tablet:group-hover:flex-col tablet:group-hover:justify-center tablet:group-hover:items-center tablet:group-hover:gap-y-[0.8rem] tablet:group-hover:absolute tablet:group-hover:top-1/2 tablet:group-hover:left-1/2 tablet:group-hover:translate-x-[-50%] tablet:group-hover:translate-y-[-50%]",
+            menuClosed: "hidden tablet:group-hover:flex tablet:group-hover:flex-col tablet:group-hover:justify-center tablet:group-hover:items-center tablet:group-hover:gap-y-[0.8rem] tablet:group-hover:absolute tablet:group-hover:top-1/2 tablet:group-hover:left-1/2 tablet:group-hover:translate-x-[-50%] tablet:group-hover:translate-y-[-50%]",
+            button: "w-full py-2 px-4 text-nowrap transition duration-100 ease-in-out tablet:group-hover:w-full tablet:group-hover:text-nowrap tablet:group-hover:py-2 tablet:group-hover:px-4 tablet:group-hover:border-1 tablet:group-hover:border-[black] tablet:group-hover:transition tablet:group-hover:duration-100 tablet:group-hover:ease-in-out tablet:group-hover:hover:scale-105 tablet:group-hover:active:scale-105",
+            toggleMenuButton: "w-6 absolute top-2 right-2 tablet:hidden",
             toggleMenuImg: "w-full h-full",
         }
     }
@@ -48,7 +49,7 @@ export default function Project(props: Project) {
                 src={imgSrc} 
                 alt={imgAlt}  />
 
-                <div className={buttonsVisible ? styling.buttons.menu : "hidden"}>
+                <div className={buttonsVisible ? styling.buttons.menu : styling.buttons.menuClosed}>
                     <button className={styling.buttons.button} onClick={() => window.open(`${githubUrl}`, '_blank')}>Github</button>
                     <button className={styling.buttons.button} onClick={handleReadMore}>More info</button>
                 </div>
--- app/_homepage/sections/myProjects.tsx
+++ app/_homepage/sections/myProjects.tsx
@@ -4,7 +4,7 @@ import projects from "@/app/_homepage/myProjects.json";
 export default function MyProjects(props: {hasHeader: boolean}) {
     const styling = {
         section: "py-[3rem] px-[5%]",
-        projectsGrid: "grid grid-cols-[repeat(2, 1fr)] gap-[2rem]"
+        projectsGrid: "grid grid-cols-[repeat(2, 1fr)] gap-8 tablet:grid-cols-[repeat(4,minmax(160px,200px))] tablet:gap-x-12 tablet:gap-y-10"
     }
 
     return (
