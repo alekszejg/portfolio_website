fe17690f789fffea6686c38f8681ea166911e0b2
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-30 20:29:18 +0100

    Now receives icon class as prop containing tailwind classes instead of scss ID

--- app/interactive-cv/sectionHeader.tsx
+++ app/interactive-cv/sectionHeader.tsx
@@ -1,10 +1,10 @@
-type SectionHeaderProps = {headerWrapperClass: string, iconID: string, icon: string, headerClass: string, header: string};
+type SectionHeaderProps = {headerWrapperClass: string, iconClass: string, icon: string, headerClass: string, header: string};
 
 export default function SectionHeader(props: SectionHeaderProps) {
-    const { headerWrapperClass, iconID, icon, headerClass, header } = props;
+    const { headerWrapperClass, iconClass, icon, headerClass, header } = props;
     return (
         <div className={headerWrapperClass}>
-            <img id={iconID} src={icon} alt="" />
+            <img className={iconClass} src={icon} alt="" />
             <h2 className={headerClass}>{header}</h2>
         </div> 
     );
