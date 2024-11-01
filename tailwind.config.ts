3b07506524ef1d19ebb9c07875fd6e2aae526510
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-01 19:23:37 +0100

    Added regular and onhover colors for cv section approaches

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
     },
     screens: {
       'tablet': '480px',
+      'cvUltrawide': '1335px',
       'ultrawide': '1400px',
     },
     extend: {
@@ -22,7 +23,9 @@ const config: Config = {
         'cvGeneralSection': 'hsl(0, 0%, 93.5%)',
         'cvGeneralHover': 'hsl(0,0%,92%,1)',
         'cvApproachSection': 'hsl(178, 75%, 93%)',
-        'cvApproachHover': 'hsl(0, 0%, 94%)',
+        'cvApproachSectionHover': 'hsl(178,85%,90%)',
+        'cvApproachItem': 'hsl(0,0%,98%)',
+        'cvApproachItemHover': 'hsl(0, 0%, 94%)',
         'cvSkillsSection': 'hsl(52, 85%, 93.5%)',
         'cvSkillsHover': 'hsl(52, 85%, 92%)',
       },
