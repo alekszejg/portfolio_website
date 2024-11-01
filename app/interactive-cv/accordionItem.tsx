7f97e7df96410c6edb8f5c810c9e5a6298b27b8a
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-01 21:21:24 +0100

    Added correct positioning of accordion arrow so it remained fixed. Edited spelling of aniamtion class so the arrow moves backwards correctly. Now interative cv is done, accordion items jsut missing 3d shadow

--- app/interactive-cv/accordionItem.tsx
+++ app/interactive-cv/accordionItem.tsx
@@ -3,21 +3,22 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function AccordionItem({ title, description }: {title: string; description: string}) {    
-    const [arrowIcon, setArrowIcon] = useState<"default" | "rotateForward" | "rotateBackwards">("default");
+    const [arrowIcon, setArrowIcon] = useState<"default" | "rotateForwards" | "rotateBackwards">("default");
     const [descriptionVisible, setVisibility] = useState(false);
     
     const handleClick = () => {
         if (arrowIcon === "default" || arrowIcon === "rotateBackwards") {
-            setArrowIcon("rotateForward");
+            setArrowIcon("rotateForwards");
             setVisibility(true);
-        } 
-        setArrowIcon("rotateBackwards");
-        setVisibility(false);
+        } else {
+            setArrowIcon("rotateBackwards");
+            setVisibility(false);
+        }
     }
 
     const styling = {
-        listItem: "py-6 pl-[1.2rem] pr-12 rounded-2xl box-border text-[clamp(1.1rem,5vw,1.2rem)] relative bg-cvApproachItem hover:bg-cvApproachItemHover hover:cursor-pointer tablet:pr-20 tablet:mt-8",
-        arrowIcon: arrowIcon === "default" ? "w-4 h-4 absolute right-7 translate-y-[-50%] select-none" : arrowIcon === "rotateForward" ? "w-4 h-4 absolute right-7 select-none animate-rotateForwards" : "w-4 h-4 absolute right-7 select-none animate-rotateBack",
+        listItem: "py-6 pl-[1.2rem] pr-12 rounded-2xl box-border text-[clamp(1.1rem,5vw,1.2rem)] bg-cvApproachItem relative hover:bg-cvApproachItemHover hover:cursor-pointer tablet:pr-20 tablet:mt-8",
+        arrowIcon: arrowIcon === "default" ? "w-4 h-4 translate-y-[-50%] absolute right-4 top-10 select-none" : arrowIcon === "rotateForwards" ? "w-4 h-4 select-none absolute right-4 top-10 animate-rotateForwards" : "w-4 h-4 absolute right-4 top-10 select-none animate-rotateBackwards",
         description: {
             wrapper: descriptionVisible ? "w-full pl-0.9rem mt-4 ml-4 border-l-1 border-black box-border" : "hidden",
             text: "text-clamp(0.95rem,5vw,1.05rem)"
@@ -26,7 +27,7 @@ export default function AccordionItem({ title, description }: {title: string; de
     
     return (
         <li className={styling.listItem} onClick={handleClick}>
-            {title}
+            {title} 
             <Image 
             className={styling.arrowIcon} 
             width={100}
@@ -34,7 +35,6 @@ export default function AccordionItem({ title, description }: {title: string; de
             src="/Icons/blackArrowDown.svg"
             alt="open or hide dropdown"
             onClick={handleClick} />
-            
             <div className={styling.description.wrapper}>
                 <p className={styling.description.text}>{description}</p>
             </div>
