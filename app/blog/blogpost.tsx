36496af0854fb5b5459503150219379e3042f864
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-01 22:33:09 +0100

    Removed blogpost.scss styling, migrated blogpost to tailwind and added blogpost type directly into the component

--- app/blog/blogpost.tsx
+++ app/blog/blogpost.tsx
@@ -1,8 +1,25 @@
-import type { PostType } from "../api/blogposts/get-recent-posts/route";
-import "@/Styling/blogpostStyle.scss";
+import Image from "next/image";
 
-export default function Blogpost(props: PostType) {
-    const {id, title, content, category_ID, created_at } = props;
+export type Blogpost = {id: number, width: string, title: string, content: string, category_ID: string, created_at: Date}
+
+export default function Blogpost(props: Blogpost) {
+    const {id, width, title, content, category_ID, created_at } = props;
+    
+    const styling = {
+        wrapper: `${width} py-2 px-4 border-2 border-[hsl(0,0%,75%)] rounded-md box-border`,
+        header: {
+            wrapper: "flex items-center gap-x-[0.8rem] mb-2",
+            profile: {
+                wrapper: "flex items-center gap-x-2",
+                imgWrapper: "w-[25px] h-[25px]",
+                img: "w-full h-full object-cover",
+                name: "text-sm"
+            }
+        },
+        title: "mb-2 text-[13px] font-medium",
+        text: "text-[12px]"
+    }
+    
     const createdAtLocal = new Date(created_at.toLocaleString());
     const dateFormat = new Intl.DateTimeFormat('en-US', {
         year: 'numeric',
@@ -16,20 +33,26 @@ export default function Blogpost(props: PostType) {
     const createdAtFormatted = dateFormat.format(createdAtLocal);
 
     return (
-        <div className="blogpost">
-            <div className="headerWrapper">
-                <div className="profileWrapper">
-                    <div className="profileImgWrapper">
-                        <img src="/Images/myPhoto.png" alt="My Photo" />
+        <div className={styling.wrapper}>
+            <div className={styling.header.wrapper}>
+                
+                <div className={styling.header.profile.wrapper}>
+                    <div className={styling.header.profile.imgWrapper}>
+                        <Image 
+                        className={styling.header.profile.img}
+                        width={100}
+                        height={100}
+                        src="/Images/myPhoto.png" 
+                        alt="My Photo" />
                     </div>
-                    <h3>Alekszej Guljajev</h3>
+                    <h3 className={styling.header.profile.name}>Alekszej Guljajev</h3>
                 </div>
                 <p className="createdAtInfo">at {createdAtFormatted}</p>
                 
             </div>
-            <h4>{title}</h4>
+            <h4 className={styling.title}>{title}</h4>
         
-            <p className="content">{content}</p>
+            <p className={styling.text}>{content}</p>
         </div>
     );
 }
