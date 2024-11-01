7f9ce4e166e1341cc07fc4267a64c38772a3ff46
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-01 23:57:41 +0100

    Now accepts both wrapper styling prop and blogpost width prop

--- app/blog/showRecentPosts.tsx
+++ app/blog/showRecentPosts.tsx
@@ -2,16 +2,19 @@ import Blogpost from "@/app/blog/blogpost";
 import type { BlogpostType } from "@/app/blog/blogpost";
 
 
-export default async function ShowRecentPosts({ width }: { width: string}) {
+export default async function ShowRecentPosts({ wrapperStyling, blogpostWidth }: {wrapperStyling: string, blogpostWidth: string}) {
+    const styling = {
+        wrapper: wrapperStyling
+    }
+
     const response = await fetch('http://localhost:3000/api/blogposts/get-recent-posts?offset=0', {cache: 'no-store'});
-    
     if (response.ok) {
         const data = await response.json();
         const posts: BlogpostType[] = data.recent_posts;
 
         return (
-            <div id="recentPostsWrapper">
-                {posts.map(post => (<Blogpost key={post.id} width={width} {...post} />))}
+            <div className={styling.wrapper}>
+                {posts.map(post => (<Blogpost key={post.id} width={blogpostWidth} {...post} />))}
             </div>
         );
     } 
