e6947d6372868ef78209bb3e5c3c5c28c6f635f3
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-01 23:46:38 +0100

    Removed a component which just acted as a wrapper that accepted specific ID. Will do it from main component directly

--- app/_homepage/stackLogoGroup.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function StackLogoGroup(props: {children: ReactNode, id: string}) {
-    return (
-        <div id={props.id}>
-            {props.children}
-        </div>
-    );
-}
