044d220311eaf16ba5555e6d3c4f76608c8b3a06
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-02 03:24:47 +0100

    Fully migrated to Tailwind. Later adjust styling and JSX. Proceed to migrating styling of projects

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,25 +1,27 @@
 import PageLayout from '@/app/_layoutComponents/pageLayout';
 import PostCreator from "./postCreator";
-import RecentPosts from "../blog/showRecentPosts";
-import "@/Styling/Pages/adminPage.scss";
+import ShowRecentPosts from "../blog/showRecentPosts";
 import ChoosePostCategory from "./choosePostCategory";
 import DisplayTotalPosts from "./displayTotalPosts";
 
 export default async function AdminPage() {
     const styling = {
-        layout: "flex flex-col gap-x-10 py-12",
-        postCreatorWrapper: "flex flex-col w-1/2"
+        layout: "flex gap-x-10 py-12",
+        postCreatorWrapper: "flex flex-col w-1/2",
+        recentPosts: {
+            section: "w-1/2",
+            wrapper: "flex flex-col gap-y-4",
+            header: "mb-6"
+        }
     };
 
     return (
         <PageLayout className={styling.layout}>
             <PostCreator wrapperStyling={styling.postCreatorWrapper} selectCategory={ChoosePostCategory()} />
-            <RecentPosts />
-            <div>
-                Testing total posta api<br/><br/>
-                <DisplayTotalPosts />
-            </div>
-            
+            <section className={styling.recentPosts.section}>
+                <h2 className={styling.recentPosts.header}>Recent posts {<DisplayTotalPosts />}</h2>
+                <ShowRecentPosts wrapperStyling={styling.recentPosts.wrapper} blogpostWidth="w-[90%]" />
+            </section>
         </PageLayout>
     );  
 }
--- app/admin/postCreator.tsx
+++ app/admin/postCreator.tsx
@@ -7,10 +7,10 @@ import handlePostSubmit from "@/actions/handlePostSubmit";
 export default function PostCreator(props: {wrapperStyling: string, selectCategory: Promise<JSX.Element | null>}) {
     const styling = {
         addPostsHeader: "flex items-end gap-x-[0.8rem] ml-[1.8rem]",
-        inputLabel: "ml-[1.8rem] mb-2",
+        inputLabel: "ml-[1.8rem] mb-2 mt-4",
         error: "ml-[0.3rem] text-[10px] text-[red]",
-        input: "h-8 px-[0.7rem] mb-6 ml-[1.8rem] border-2 border-hsl(0,0%,75%)] rounded-md box-border focus:border-hsl(0,0%,65%) focus:outline-none",
-        textArea: "h-[200px] pt-2 px-[0.7rem] pb-[1.2rem] ml-[1.8rem] mb-[0.8rem] border-2 border-[hsl(0,0%,75%)] rounded-md box-border resize-none focus:border-hsl(0,0%,65%) focus:outline-none",
+        input: "h-8 px-[0.7rem] mb-6 ml-[1.8rem] border-2 border-[hsl(0,0%,75%)] rounded-md box-border focus:border-[hsl(0,0%,65%)] focus:outline-none",
+        textArea: "h-[200px] pt-2 px-[0.7rem] pb-[1.2rem] ml-[1.8rem] mb-[0.8rem] border-2 border-[hsl(0,0%,75%)] rounded-md box-border resize-none focus:border-[hsl(0,0%,65%)] focus:outline-none",
         submitButton: "flex justify-center items-center self-end w-[130px] py-2 rounded-md border-2 border-[hsl(0,0%,75%)] font-medium tracking-wide hover:scale-[1.01]",
         submittedMessage: "self-center text-[green]"
     };
