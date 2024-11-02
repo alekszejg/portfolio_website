88e42b2301b1d41872c828c84e070e3c65c91fb3
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-02 23:57:41 +0100

    Added styling through imported styling object, adjusted props (removed img id, wrapperID, now only do styles based on imgType prop

--- app/projects/featurePanel.tsx
+++ app/projects/featurePanel.tsx
@@ -1,27 +1,31 @@
 import Image from "next/image";
 import styling from "@/app/projects/styling";
 
-export type ImageWrapper = "imgWrapper" | "svgImgWrapper";
+export type ImageType = "regular" | "svg";
 
 interface ProjectFeatureProps {
-    imgWrapperClass: ImageWrapper,
+    imgType: ImageType,
     imgSide: string,
     imgSrc: string,
     imgAlt: string,
     header: string,
     text: string,
-    imgWrapperID?: string,
-    imgID?: string,
+    imgExtraStyling?: string
 }
 
 export default function ProjectFeaturePanel(props: ProjectFeatureProps) {
 
-    const {imgWrapperClass, imgSide, imgSrc, imgAlt, header, text, imgWrapperID, imgID } = props;
+    const {imgType, imgSide, imgSrc, imgAlt, header, text, imgExtraStyling } = props;
 
     return (
         <div className={`${styling.panel.wrapper} ${imgSide === "left" ? "bg-blueProjectPanel" : "bg-yellowProjectPanel"}`}>
-            <div className={imgSide === "right" ? `${imgWrapperClass} imgWrapperRight` : imgWrapperClass} id={imgWrapperID}>
-                <img className="panelImage" id={imgID} src={imgSrc} alt={imgAlt} />
+            <div className={imgSide === "right" ? `${imgType === "regular" ? `${styling.panel.imgWrapper} order-2` : `${styling.panel.svgImgWrapper} order-2`}` : `${imgType === "regular" ? styling.panel.imgWrapper : styling.panel.svgImgWrapper}`}>
+                <Image 
+                className={imgType === "regular" ? styling.panel.img : styling.panel.svgImg} 
+                width={1000}
+                height={1000}
+                src={imgSrc} 
+                alt={imgAlt} />
             </div>
             <div className={styling.panel.textWrapper}>
                 <h2 className={styling.panel.header}>{header}</h2>
