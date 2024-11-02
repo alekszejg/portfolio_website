e7d45a56c815397c4412c6c4836d9ffeb3c0dee9
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-02 03:11:24 +0100

    Now sends wrapper styling prop to postcreator with tailwind styling

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,15 +1,19 @@
 import PageLayout from '@/app/_layoutComponents/pageLayout';
 import PostCreator from "./postCreator";
-import RecentPosts from "./showRecentPosts";
+import RecentPosts from "../blog/showRecentPosts";
 import "@/Styling/Pages/adminPage.scss";
 import ChoosePostCategory from "./choosePostCategory";
 import DisplayTotalPosts from "./displayTotalPosts";
 
 export default async function AdminPage() {
+    const styling = {
+        layout: "flex flex-col gap-x-10 py-12",
+        postCreatorWrapper: "flex flex-col w-1/2"
+    };
 
     return (
-        <PageLayout layoutID="adminPageLayout">
-            <PostCreator selectCategory={ChoosePostCategory()} />
+        <PageLayout className={styling.layout}>
+            <PostCreator wrapperStyling={styling.postCreatorWrapper} selectCategory={ChoosePostCategory()} />
             <RecentPosts />
             <div>
                 Testing total posta api<br/><br/>
