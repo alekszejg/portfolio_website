97996eda0090d67deb952cdf3f83cc13147b00ef
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-02 03:11:53 +0100

    Migrated to tailwind from scss

--- app/admin/choosePostCategory.tsx
+++ app/admin/choosePostCategory.tsx
@@ -2,11 +2,15 @@ import type { CategoryData } from "@/app/api/blogposts/all-categories/route";
 
 
 export default async function ChoosePostCategory() {
+    const styling = {
+        select: "relative bottom-[3px] border-hsl(0,0%,65%) focus:border-hsl(0,0%,65%) focus:outline-none" 
+    };
+
     const response = await fetch('http://localhost:3000/api/blogposts/all-categories');
     if (response.ok) {
         const data = await response.json();
         return (
-            <select name="category"><option value={undefined} />
+            <select className={styling.select} name="category"><option value={undefined} />
             {data.allCategories.map((category: CategoryData) => (
                 <option key={category.id} value={category.id}>{category.category}</option>
             ))}
