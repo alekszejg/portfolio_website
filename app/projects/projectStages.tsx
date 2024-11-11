43294062c771068c4dabb760f4d2876687a03f42
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-11 15:26:46 +0100

    Finished rough styling for project stages progress bar. Now need to do image carousel next to it

--- app/projects/projectStages.tsx
+++ app/projects/projectStages.tsx
@@ -3,29 +3,64 @@ import { useState } from "react";
 
 export default function ProjectStages({stages}: {stages: {stage: string, text: string}[]}) {
     const styling = {
+        wrapper: "flex justify-between",
         progressBar: "flex flex-col justify-between w-[70px] h-[585px] bg-[hsl(0,0,95%)] rounded-full shadow-[1px_1px_10px] shadow-[#BEBEBE] relative left-10",
-        button: "w-[70px] h-[70px] rounded-full bg-[hsl(0,0,92%)]"
+        progressBarColored: "w-full rounded-full shadow-[1px_1px_10px] shadow-[#BEBEBE] absolute",
+        button: "w-[70px] h-[70px] rounded-full bg-[hsl(0,0,92%)] z-50",
+        activeButton: "w-[70px] h-[70px] rounded-full bg-red-500 z-50"
     };
 
-    const [index, setIndex] = useState<number>(0);
+    const [stageIndex, setIndex] = useState<number>(0);
 
     const handleClick = (newIndex: number) => {
-        if (newIndex !== index) {
-            setIndex(index);
+        if (stageIndex !== newIndex) {
+            console.log("change detected")
+            setIndex(newIndex);
+        }
+    }
+
+    const getDynamicProgress = () => {
+        if (stages.length === 3) {
+            switch (stageIndex) {
+                case 0:
+                    return "h-full bg-transparent"
+                case 1: 
+                    return "h-[calc(50%+2rem)] bg-green-500"
+                case 2:
+                    return "h-full bg-green-500"
+            }
+        }
+
+        else if (stages.length === 4) {
+            switch (stageIndex) {
+                case 0:
+                    return "h-full bg-transparent"
+                case 1: 
+                    return "h-[calc(100%/3+2rem)] bg-green-500"
+                case 2:
+                    return "h-[calc(100%/3*2+1rem)] bg-green-500"
+                case 3:
+                    return "h-full bg-green-500"
+            }
         }
     }
 
     return (
-        <>
-        <div className={styling.progressBar}>
-            {stages.map((stage, index) => (
-                <button className={styling.button} onClick={() => handleClick(index)}/>
-            ))}
+        <div className={styling.wrapper}>
+
+            <div className={styling.progressBar}>
+                {stages.map((stage, index) => (
+                    <button 
+                        className={index === stageIndex ? styling.activeButton : styling.button} 
+                        onClick={() => handleClick(index)} />
+                ))}
+                <div className={`${styling.progressBarColored} ${getDynamicProgress()}`} />
+            </div>
+
+            <div>
+                <h3>{stages[stageIndex].stage}</h3>
+                <p>{stages[stageIndex].text}</p>
+            </div>
         </div>
-        <div>
-            <h3>{stages[index].stage}</h3>
-            <p>{stages[index].text}</p>
-        </div>
-        </>
     );
 }
