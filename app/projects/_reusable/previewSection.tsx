c2c41309105b646da8ad1724e9044e90349295c1
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-12 19:41:15 +0100

    For now removed hero section and renamed it to previewSection. I will still be adding a hero section in a bit which will be better styled, already have something in mind

--- app/projects/_reusable/previewSection.tsx
+++ app/projects/_reusable/previewSection.tsx
@@ -2,13 +2,13 @@ import ImageCarousel from "@/app/projects/_reusable/imageCarousel";
 import ProjectStages from "@/app/projects/_reusable/projectStages";
 import type { ImageArray } from "@/app/projects/_reusable/imageCarousel";
 
-type HeroSectionProps = {
+type PreviewSectionProps = {
     images: ImageArray,
     stages: {stage: string, text: string}[],
     wrapperStyling: string
 }
 
-export default function HeroSection({ images, stages, wrapperStyling}: HeroSectionProps) {
+export default function PreviewSection({ images, stages, wrapperStyling}: PreviewSectionProps) {
     const styling = {
         imageCarouselWrapper: "flex justify-between items-center",
         stagesWrapper: "flex justify-between gap-x-6"
