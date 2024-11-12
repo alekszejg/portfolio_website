0ff7a094f6972b7f193c4d888d71f16e4e20f879
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-12 17:09:17 +0100

    Removed confusing wrapper, added another one for desktop. Fixed desktop styling of logos and text, adjusted mobile styling

--- app/_homepage/sections/myStack.tsx
+++ app/_homepage/sections/myStack.tsx
@@ -13,9 +13,10 @@ export default function MyStack(props: {hasHeader: boolean}) {
     const styling = {
         section: "py-[3rem] px-[5%]",
         header: "mb-[2rem]",
-        stackWrapper: "flex gap-x-[3rem] tablet:gap-x-[1.6rem] tablet:shrink-0 tablet:w-[30%]",
-        mobileGrid: "grid grid-cols-[repeat(auto-fill,1.5rem)] gap-[1rem] w-[4rem] tablet:hidden",
-        desktopGrid: "hidden tablet:flex tablet:flex-col tablet:gap-x-[1.2rem] tablet:shrink-0",
+        stackWrapper: "flex gap-x-[3rem] tablet:gap-x-[1.6rem] tablet:shrink-0",
+        mobileGrid: "grid grid-cols-[repeat(2,1.5rem)] gap-[1rem] w-16 h-[1px] tablet:hidden", // leave h-[1px], it overrites automatic h-full
+        desktopLogosWrapper: "hidden tablet:flex tablet:flex-col tablet:items-center tablet:gap-y-[1.6rem] tablet:shrink-0",
+        desktopLogoRow: "hidden tablet:flex tablet:gap-x-[1.2rem]",
         description: {
             wrapper: "tablet:w-[70%]",
             header: "mb-[0.8rem]",
@@ -42,36 +43,36 @@ export default function MyStack(props: {hasHeader: boolean}) {
     
     
     return (
-        <section id="stackSection">
-            {props.hasHeader && <h2>My stack</h2>}
+        <section className={styling.section}>
+            {props.hasHeader && <h2 className={styling.header}>My stack</h2>}
             <div className={styling.stackWrapper} ref={stackSectionRef}>
-                
-                <div id="stackLogosWrapper">
-                    
-                    <div className={styling.mobileGrid}>
-                        {Object.keys(myStack).map(skill => (
-                            <TechSkillLogo searchParamName={skill} selectedSkill={selectedSkill} imgSrc={stack[skill].image} imgAlt={stack[skill].alt} />
-                        ))}
-                    </div>
+                        
+                <div className={styling.mobileGrid}>
+                    {Object.keys(myStack).map(skill => (
+                        <TechSkillLogo searchParamName={skill} selectedSkill={selectedSkill} imgSrc={stack[skill].image} imgAlt={stack[skill].alt} />
+                    ))}
+                </div>
 
-                    <div className={styling.desktopGrid}>
+                <div className={styling.desktopLogosWrapper}>
+                    <div className={styling.desktopLogoRow}>
                         <TechSkillLogo searchParamName="html" selectedSkill={selectedSkill} imgSrc={stack["html"].image} imgAlt={stack["html"].alt} />
                         <TechSkillLogo searchParamName="css" selectedSkill={selectedSkill} imgSrc={stack["css"].image} imgAlt={stack["css"].alt} />
                         <TechSkillLogo searchParamName="sass/scss" selectedSkill={selectedSkill} imgSrc={stack["sass/scss"].image} imgAlt={stack["sass/scss"].alt} />
                         <TechSkillLogo searchParamName="tailwind-css" selectedSkill={selectedSkill} imgSrc={stack["tailwind-css"].image} imgAlt={stack["tailwind-css"].alt} />
                     </div>
 
-                    <div className={styling.desktopGrid}>
+                    <div className={styling.desktopLogoRow}>
                         <TechSkillLogo searchParamName="javascript" selectedSkill={selectedSkill} imgSrc={stack["javascript"].image} imgAlt={stack["javascript"].alt} />
                         <TechSkillLogo searchParamName="typescript" selectedSkill={selectedSkill} imgSrc={stack["typescript"].image} imgAlt={stack["typescript"].alt} />
                         <TechSkillLogo searchParamName="python" selectedSkill={selectedSkill} imgSrc={stack["python"].image} imgAlt={stack["python"].alt} />
                     </div>
 
-                    <div className={styling.desktopGrid}>
+                    <div className={styling.desktopLogoRow}>
                         <TechSkillLogo searchParamName="reactjs" selectedSkill={selectedSkill} imgSrc={stack["reactjs"].image} imgAlt={stack["reactjs"].alt} />
                         <TechSkillLogo searchParamName="nextjs" selectedSkill={selectedSkill} imgSrc={stack["nextjs"].image} imgAlt={stack["nextjs"].alt} />
                     </div>
-                </div>
+                </div>  
+               
 
                 <div className={styling.description.wrapper}>
                     <h3 className={styling.description.header}>{stack[selectedSkill as keyof StackType || "html"].name}</h3>
