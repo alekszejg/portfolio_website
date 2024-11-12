a4928e30a649ff274a4855bca55643196b501a9e
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-12 17:11:51 +0100

    Adjusted dimensions and some logic

--- app/projects/_reusable/projectStages.tsx
+++ app/projects/_reusable/projectStages.tsx
@@ -12,14 +12,15 @@ export default function ProjectStages({ stages, wrapperStyling }: ProjectStagesP
         progressBar: "flex flex-col justify-between self-center w-[25px] h-2/3 bg-[hsl(0,0,95%)] rounded-full shadow-[1px_1px_10px] shadow-[#BEBEBE] relative",
         progressBarColored: "w-full rounded-full shadow-[1px_1px_10px] shadow-[#BEBEBE] absolute",
         button: "w-[25px] h-[25px] rounded-full bg-[hsl(0,0,92%)] z-50",
-        activeButton: "w-[25px] h-[25px] rounded-full bg-red-500 z-50"
+        activeButton: "w-[25px] h-[25px] rounded-full bg-red-500 z-50",
+        header: "font-medium tracking-wider",
+        text: "text-sm"
     };
 
     const [stageIndex, setIndex] = useState<number>(0);
 
     const handleClick = (newIndex: number) => {
         if (stageIndex !== newIndex) {
-            console.log("change detected")
             setIndex(newIndex);
         }
     }
@@ -63,8 +64,8 @@ export default function ProjectStages({ stages, wrapperStyling }: ProjectStagesP
             </div>
 
             <div>
-                <h3>{stages[stageIndex].stage}</h3>
-                <p>{stages[stageIndex].text}</p>
+                <h3 className={styling.header}>{stages[stageIndex].stage}</h3>
+                <p className={styling.text}>{stages[stageIndex].text}</p>
             </div>
         </div>
     );
