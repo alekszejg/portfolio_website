11dcab226bdbd17eae1e8766e9a36ef46a609569
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-16 11:25:09 +0100

    Renamed urlPaths.json file to url.paths.ts. Adjusted its structure to fit all URLs including pages for my projects (with github url and local path on my website)

---
+++ app/url.paths.ts
@@ -0,0 +1,25 @@
+const urlPaths = {
+    homepage: "/",
+    cv: "/interactive-cv",
+    projects: {
+       base: "/projects",
+
+       discordBot: {
+        githubUrl: "https://github.com/alekszejg/discord_bot_python",
+        localPath: "/projects/discord-bot",
+       },
+
+       portfolioWebsite: {
+        githubUrl: "https://github.com/alekszejg/portfolio_website",
+        localPath: "/projects/portfolio-website",
+       },
+
+       mangopostWebsite: {
+        githubUrl: "https://github.com/alekszejg/mangopost_nextjs",
+        localPath: "/projects/mangopost-website",
+       }
+    }
+};
+
+
+export default urlPaths;
