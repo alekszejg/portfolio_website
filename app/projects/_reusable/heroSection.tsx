02cfe955e1b4effbec2fca0d26c71044facbf53b
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-16 04:58:07 +0100

    Added hero section (wrapper) styling into styling object and imported it for heroSection component to prevent sending same props for every project page. Still imgWrapper for each project will prob have unique styling specified in prop. Now need to develop consistant styling for hero section.

--- app/projects/_reusable/heroSection.tsx
+++ app/projects/_reusable/heroSection.tsx
@@ -1,15 +1,36 @@
-type HeroSectionProps = {wrapperStyling: string, header: string, text: string, imgSrc: string, imgAlt: string}
+import Image from "next/image";
+import globalStyling from "@/app/projects/styling";
 
-export default function HeroSection({ wrapperStyling, header, text, imgSrc, imgAlt }: HeroSectionProps) {
+
+type HeroSectionProps = {
+    imgWrapperStyling: string, 
+    header: string, 
+    text: string, 
+    imgSrc: string, 
+    imgAlt: string
+};
+
+
+export default function HeroSection({ imgWrapperStyling, header, text, imgSrc, imgAlt }: HeroSectionProps) {
     const styling = {
+        section: globalStyling.heroSection,
         header: "",
         text: "",
+        image: "w-full h-full"
     }
 
     return (
-        <section className={wrapperStyling}>
+        <section className={styling.section}>
             <h2 className={styling.header}>{header}</h2>
             <p className={styling.text}>{text}</p>
+            <div className={imgWrapperStyling}>
+                <Image 
+                className={styling.image}
+                width={1000} 
+                height={1000} 
+                src={imgSrc} 
+                alt={imgAlt} />
+            </div>
         </section>
     );
 }
