97513d6c3cf8a285942d58f892c950f58ceed52f
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-17 23:12:37 +0100

    Removed unused and complicated styling from homepage when trying to adapt interactive project previews for both homepage and /projects routes. Added new props which were also added recently

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,14 @@
 import PageLayout from '@/app/_layoutComponents/pageLayout';
 import BasicInfo from '@/app/_homepage/basicInfo/basicInfo';
 import ResponsiveUI from '@/app/_homepage/responsiveUI';
-import RecentProjects from '@/app/_homepage/recentProjects/recentProjects';
+import ProjectsSection from '@/app/projects/projectsSection';
 import ShowRecentPosts from "@/app/blog/showRecentPosts";
 
 export default async function Homepage() {
     const styling = {
         layout: "max-w-[1400px] relative ultrawide:mx-auto",
-        mainWrapper: "block tablet:hidden",
-        mobileUI: {
-            recentPostsWrapper: "flex flex-col gap-y-6"
-        },
-        desktopUI: {
-            wrapper: "hidden tablet:block",
-            recentPosts: {
-                section: "py-12 box-border",
-                wrapper: "flex flex-col gap-y-6",
-                header: "mb-4"
-            }
-        }
+        recentProjectsSection: "py-12 px-[5%]",
+        recentPostsSection: "py-12 px-[5%]",
     }
 
     return (
@@ -26,8 +16,8 @@ export default async function Homepage() {
             <BasicInfo />
             
             <ResponsiveUI 
-            RecentProjects={<RecentProjects />} 
-            RecentPosts={<ShowRecentPosts wrapperStyling={styling.mobileUI.recentPostsWrapper} blogpostWidth={"w-3/4"} />} 
+            ProjectsSection={<ProjectsSection display="recent" wrapperStyling={styling.recentProjectsSection} includeHeader={true} />} 
+            ShowRecentPosts={<ShowRecentPosts wrapperStyling={styling.recentPostsSection} includeHeader={true} blogpostWidth="w-3/4" />} 
             />
             
         </PageLayout>
