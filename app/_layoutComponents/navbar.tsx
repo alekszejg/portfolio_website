785bb8e5b522f260193285eadef1c5f9149652d1
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-19 17:47:46 +0100

    Fixed navbar shrinking on desktop and gave it higher z-index to be above all elements

--- app/_layoutComponents/navbar.tsx
+++ app/_layoutComponents/navbar.tsx
@@ -29,22 +29,28 @@ export default function Navbar() {
 
     const styling = {
         nav: "flex justify-between items-center max-w-[1400px] h-[3rem] relative bg-[white] ultrawide:mx-auto",
-        burgerMenuButton: "h-[1.6rem] p-0 mx-[0.6rem] bg-transparent border-0 tablet:hidden",
-        burgerMenuImg: "w-full h-full",
-        navListMobile: burgerMenuOpened ? "flex flex-col w-3/5 h-screen p-0 m-0 absolute top-full bg-[rgba(190,190,190,0.95)]" : "hidden",
-        navListPC: "tablet:flex tablet:flex-row tablet:w-[30%] tablet:h-full tablet:ml-[1rem] tablet:static tablet:bg-inherit",
-        navListItem: "flex justify-center items-center w-full h-[3rem] hover:bg-[rgba(150,150,150,0.7)] tablet-h-full",
-        navLink: "flex justify-center items-center w-full h-full text-white font-medium tracking-wider tablet:inline tablet:h-auto tablet:text-center tablet:text-nowrap tablet:text-black tablet:text-[1.1rem] tablet:opacity-50",
-        logoLink: "w-2/5 h-full tablet:w-[min(150px,20%)]",
-        logoImg: "w-full h-full"
+        burgerMenu: {
+            button: "h-[1.6rem] p-0 mx-[0.6rem] bg-transparent border-0 tablet:hidden",
+            img: "w-full h-full"
+        },
+        navMenu: {
+            listMobile: burgerMenuOpened ? "flex flex-col w-3/5 h-screen p-0 m-0 absolute top-full bg-[rgba(190,190,190,0.95)] z-50" : "hidden",
+            listPC: "tablet:flex tablet:flex-row tablet:w-[30%] tablet:h-full tablet:static tablet:bg-inherit",
+            listItem: "flex justify-center items-center w-full h-12 hover:bg-[rgba(150,150,150,0.7)] tablet-h-full tablet:mx-4 tablet:hover:bg-inherit",
+            link: "flex justify-center items-center w-full h-full text-white font-bold tracking-wider tablet:inline tablet:h-auto tablet:text-center tablet:text-nowrap tablet:text-black tablet:text-[1.1rem] tablet:opacity-40 tablet:hover:opacity-60"
+        },
+        logo: {
+            link: "w-2/5 h-full tablet:w-[min(150px,20%)]",
+            img: "w-full h-full"
+        }
     }
 
     return (
         <nav className={styling.nav}> 
-            <button className={styling.burgerMenuButton}>
+            <button className={styling.burgerMenu.button}>
                 <Image 
                 ref={burgerIconRef}
-                className={styling.burgerMenuImg}
+                className={styling.burgerMenu.img}
                 width={0}
                 height={0} 
                 src="/Icons/burgerMenuIcon.svg" 
@@ -52,18 +58,18 @@ export default function Navbar() {
                 onClick={toggleBurgerMenu} />
             </button>
             
-            <ul className={`${styling.navListMobile} ${styling.navListPC}`}>
-                <li className={styling.navListItem}>
-                    <Link href={urlPaths.cv} className={styling.navLink}>Interactive CV</Link>
+            <ul className={`${styling.navMenu.listMobile} ${styling.navMenu.listPC}`}>
+                <li className={styling.navMenu.listItem}>
+                    <Link href={urlPaths.cv} className={styling.navMenu.link}>Interactive CV</Link>
                 </li>
-                <li className={styling.navListItem}>
-                    <Link href={urlPaths.projects.base} className={styling.navLink}>Projects</Link>
+                <li className={styling.navMenu.listItem}>
+                    <Link href={urlPaths.projects.base} className={styling.navMenu.link}>Projects</Link>
                 </li>
             </ul>
         
-            <Link className={styling.logoLink} href="/">
+            <Link className={styling.logo.link} href="/">
                 <Image 
-                    className={styling.logoImg} 
+                    className={styling.logo.img} 
                     width={0} 
                     height={0} 
                     src="data:,"
