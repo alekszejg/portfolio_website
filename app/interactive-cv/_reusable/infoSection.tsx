f74d68ff2848ad1862aca5875537b2c82ef68641
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-11-27 19:10:49 +0100

    Added div wrapper to InfoSection instead of it being inside parent component to simplify JSX

--- app/interactive-cv/_reusable/infoSection.tsx
+++ app/interactive-cv/_reusable/infoSection.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from "react";
 import SectionHeader from "./header";
 
 interface InfoSectionProps {
+    wrapperStyling: string,
     headerWrapperClass?: string;
     iconClass: string;
     headerIconSrc: string;
@@ -14,29 +15,31 @@ interface InfoSectionProps {
 }
 
 export default function InfoSection(props: InfoSectionProps) {
-    const { infoLoop, infoListClass } = props;
+    const { wrapperStyling, infoLoop, infoListClass } = props;
     const { headerWrapperClass, iconClass, headerIconSrc, headerClass, header } = props;
+    
     const styling = {
+        wrapper: wrapperStyling,
         headerWrapper: "flex justify-center items-center w-full pb-2 border-b-2 border-black",
     }
 
 
     return (
-        <>
-        <SectionHeader 
-        wrapperStyling={headerWrapperClass ? `${styling.headerWrapper} ${headerWrapperClass}` : styling.headerWrapper} 
-        icon={{styling: iconClass, svg: headerIconSrc}} 
-        header={{styling: headerClass, text: header}} />
-        
-        {props.infoLoop && 
-            <ul className={infoListClass}>
-                {infoLoop}
-            </ul>
-        }
+        <div className={styling.wrapper}>
+            <SectionHeader 
+            wrapperStyling={headerWrapperClass ? `${styling.headerWrapper} ${headerWrapperClass}` : styling.headerWrapper} 
+            icon={{styling: iconClass, svg: headerIconSrc}} 
+            header={{styling: headerClass, text: header}} />
+            
+            {props.infoLoop && 
+                <ul className={infoListClass}>
+                    {infoLoop}
+                </ul>
+            }
 
-        {props.description && 
-            <p className={props.descriptionClass}>{props.description}</p>
-        }
-        </>
+            {props.description && 
+                <p className={props.descriptionClass}>{props.description}</p>
+            }
+        </div>
     );
 }
