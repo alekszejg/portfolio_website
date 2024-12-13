b5880ba044ea2a6025d75153553d141715de37e4
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-13 14:37:00 +0100

    .env variables are now more descriptive

--- postgres.ts
+++ postgres.ts
@@ -4,10 +4,10 @@ dotenv.config();
 
 
 export let pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
 });
 
