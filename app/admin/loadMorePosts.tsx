c2a9a0a4fa1c5a454d3c3c88e670c1b04f3c1bf2
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-13 09:12:12 +0100

    Forgot to save file

--- app/admin/loadMorePosts.tsx
+++ app/admin/loadMorePosts.tsx
@@ -1,21 +1,21 @@
 "use client"
 
 import { useState } from "react";
-import Blogpost from "../blog/blogpost";
-import type { PostType } from "../api/blogposts/get-recent-posts/route";
+import Blogpost from "@/app/blog/blogpost";
+import type { BlogpostType } from "@/app/blog/blogpost";
 
 
-export default function LoadMorePosts({ initialPosts }: { initialPosts: PostType[] }) {
-    const [posts, setPosts] = useState<PostType[]>(initialPosts);
+export default function LoadMorePosts({ initialPosts }: { initialPosts: BlogpostType[] }) {
+    const [posts, setPosts] = useState<BlogpostType[]>(initialPosts);
     const [offset, setOffset] = useState(10);
 
     const viewMorePosts = async () => {
         const newOffset = offset + 10;
         setOffset(newOffset);
-        const response = await fetch(`http://localhost:3000/api/blogposts/get-recent-posts?offset=${newOffset}`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/blogposts/get-recent-posts?offset=${newOffset}`);
         if (response.ok) {
             const data = await response.json();
-            const newPosts: PostType[] = data.recent_posts;
+            const newPosts: BlogpostType[] = data.recent_posts;
             setPosts((prevPosts) => [...prevPosts, ...newPosts]);
         }
     }
