798151b2119a8793e813981e9136f599824c8e4d
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-13 20:08:24 +0100

    Moved GET /api/blogposts/get-all-post-ids logic into getStaticParams() of blog/[postID] to prevent fetching errors during the next build

--- app/blog/[postID]/page.tsx
+++ app/blog/[postID]/page.tsx
@@ -1,8 +1,10 @@
-import Blogpost from "../blogpost";
+import Blogpost from "@/app/blog/blogpost";
+import type { PoolClient } from "pg";
+import { pool } from "@/postgres";
 
 
 export async function generateStaticParams() {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/blogposts/get-all-post-ids`);
+    /*const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/blogposts/get-all-post-ids`);
     
     if (response.ok) {
         const data = await response.json();
@@ -10,13 +12,28 @@ export async function generateStaticParams() {
     } else {
         console.error("Failed to fetch total posts", response.statusText);
         return [];
+    }*/
+
+    let client: PoolClient | null = null;
+
+    try {
+        client = await pool.connect();
+        const response = await client.query('SELECT * FROM posts_category');
+        return response.rows;
+    } 
+    
+    catch {
+        return [];
+    } 
+    
+    finally {
+        client && client.release();
     }
 }
 
 
 export default async function BlogpostPage(props: {params: Promise<{postID: string}>}) {
     const params = await props.params;
-    console.log("parameter is ", params.postID)
     const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/blogposts/post-data?postID=${params.postID}`);
 
     if (response.ok) {
