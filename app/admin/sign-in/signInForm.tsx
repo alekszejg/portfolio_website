df4e2645cb669d649ae6f20c4106060d02d38374
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-15 03:03:37 +0100

    Renamed and extended verifyCaptcha component to also include all logic for checking submitted formData before activating NextAuth signin function. Everything works expect for invalid credentials I get useless stupid NextAuth 30 line error in server console. Gonna leave it for now

--- app/admin/sign-in/signInForm.tsx
+++ app/admin/sign-in/signInForm.tsx
@@ -3,8 +3,7 @@ import { useEffect, useRef, FormEvent, useState } from 'react';
 import ReCAPTCHA, { ReCAPTCHAInstance } from 'react-google-recaptcha';
 import { useRouter } from 'next/navigation';
 import { useSession, signIn } from 'next-auth/react';
-import verifyCaptcha from '@/app/_actions/verifyCaptcha';
-import sanitizeInput from '@/app/_actions/sanitizeInput';
+import handleAdminSignin from '@/app/_actions/handleAdminSignin';
 
 
 export default function SignInForm() {
@@ -15,8 +14,6 @@ export default function SignInForm() {
         submitButton: "flex justify-center items-center w-[150px] py-3 mt-[0.1rem] border-2 border-formInput rounded-lg font-medium tracking-wide hover:scale-[1.01]"
     }
 
-    let error = "";
-
     const { status } = useSession();
     const router = useRouter();
 
@@ -26,63 +23,53 @@ export default function SignInForm() {
     }, [status, router]);
 
 
-    
-    const [errors, setErrors] = useState({username: "", password: "", captcha: ""});
+    const [errors, setErrors] = useState({inputs: "", auth: ""});
     const captchaRef = useRef<ReCAPTCHAInstance | null>(null);
 
     const handleSubmit = async (event: FormEvent) => {
         event.preventDefault();
-
+        
         const formData = new FormData(event.currentTarget as HTMLFormElement);
-        const rawUsername = formData.get('username') as string;
-        const rawPassword = formData.get('password') as string;
-
-        const tempCaptchaValue = captchaRef.current?.getValue(); 
-
-        if (!tempCaptchaValue) {
-            setErrors({...errors, captcha: "Failed to obtain Captcha"});
-            return;
-        }
-    
-        const response = await verifyCaptcha(tempCaptchaValue);
-        if (!response.verified) {
-            setErrors({...errors, captcha: response.error}); 
-            return;
-        }
-
-        const [cleanUsername, cleanPassword] = await sanitizeInput([rawUsername, rawPassword]);
-        !cleanUsername && setErrors({...errors, username: "Illegal characters in username"});
-        !cleanPassword && setErrors({...errors, password: "Illegal characters in password"});
-        if (!cleanUsername || !cleanPassword) return;
-
+        const tempCaptchaValue = captchaRef.current?.getValue();
+        const { inputsError, captchaError } = await handleAdminSignin(formData, tempCaptchaValue);
         
-        const result = await signIn('credentials', {
-            redirect: false,
-            username: cleanUsername,
-            password: cleanPassword,
-        });
+        if (inputsError) {
+            setErrors({...errors, inputs: inputsError});
+            return;
+        }
 
-        // handle errors later
-        if (result && result.error) {
-            error = result.error;
+        if (captchaError) {
+            setErrors({...errors, inputs: captchaError});
             captchaRef.current?.reset();
-        } else if (result && result.ok) {
-            router.push('/admin');
-        } else {
-            error = "Unexpected error occurred."
+            return;
         }
-        
+
+    
+        const response = await signIn('credentials', {
+            redirect: false,
+            username: formData.get('username') as string,
+            password: formData.get('password') as string,
+        });
+    
+        if (response && response.error) {
+            setErrors({...errors, auth: "Invalid credentials. Try again"});
+            captchaRef.current?.reset();
+            return;
+        } 
+
+        router.push('/admin')  
     };
 
 
     return (
         <form className={styling.form} onSubmit={handleSubmit}>
-            {error && <p className={styling.error}>{error}</p>}
-        
             <input className={styling.inputField} type="text" placeholder="Username" name="username" /> 
             <input className={styling.inputField} type="password" placeholder="Password" name="password" /> 
             <ReCAPTCHA ref={captchaRef} sitekey={process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY!} />
             <button className={styling.submitButton} type="submit">Submit</button>
+            
+            {errors.inputs && <p className={styling.error}>{errors.inputs}</p>}
+            {errors.auth && <p className={styling.error}>{errors.auth}</p>}
         </form>
     );
 }
