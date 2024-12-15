a58ae346547ea43be298831f10481fd95ca074e9
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-15 17:23:50 +0100

    Added HTTP headers for security, ensured CSP header works (in dev mode its normal to get uncaught evalerror for react-refresh-utils, but it goes away during production in 'build' mode. So far so good...

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,35 @@
 
 const nextConfig = {
     reactStrictMode: false,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=31536000; includeSubDomains; preload',
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: "default-src 'self'; script-src 'self' https://www.google.com https://www.gstatic.com https://www.recaptcha.net 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https://www.google.com https://www.gstatic.com data:; font-src 'self'; frame-src https://www.google.com https://www.recaptcha.net; connect-src 'self' https://www.google.com; object-src 'none'; base-uri 'self'; form-action 'self';",
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'X-Frame-Options', 
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'no-referrer',
+                    },
+                ]
+            }
+        ]
+    },
 };
 
 export default nextConfig;
