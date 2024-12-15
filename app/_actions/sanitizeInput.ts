9dd4d13d85e4597848da0a667cca918cac101675
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-15 03:01:20 +0100

    Renamed and moved sanitizeInput function to utils folder since it isnt asynchronous and pointless to have as separate server action, also merged it with illegal character replacer, still might have to do some debugging and look for possible errors

--- app/_actions/sanitizeInput.ts
+++
@@ -1,8 +0,0 @@
-"use server"
-
-import sanitize from "sanitize-html"
-
-export default async function sanitizeInput(input: string[]) {
-    const cleanInput = input.map(value => sanitize(value));
-    return cleanInput;
-}
