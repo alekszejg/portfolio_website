9fc3599a689356454f1b7fc65c7377ede4f09466
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-16 17:45:44 +0100

    Had to remove CSP header, since while inline scripts were allowed it broke all my event handlers entirely, which needs more debugging later

--- next.config.mjs
+++ next.config.mjs
@@ -13,10 +13,6 @@ const nextConfig = {
                         value: 'max-age=31536000; includeSubDomains; preload',
                     },
                     {
-                        key: "Content-Security-Policy",
-                        value: "default-src 'self'; script-src 'self' https://www.google.com https://www.gstatic.com https://www.recaptcha.net 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https://www.google.com https://www.gstatic.com data:; font-src 'self'; frame-src https://www.google.com https://www.recaptcha.net; connect-src 'self' https://www.google.com; object-src 'none'; base-uri 'self'; form-action 'self';",
-                    },
-                    {
                         key: 'X-Content-Type-Options',
                         value: 'nosniff',
                     },
