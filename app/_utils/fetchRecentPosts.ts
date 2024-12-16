42379843fc65419fb1b3d6d75925f52a9bd5d4c5
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-16 18:02:34 +0100

    Added server-side recent posts fetching which is temporary cached, doesnt conflict with build version of nextjs and doesnt involve server actions for data fetching

---
+++ app/_utils/fetchRecentPosts.ts
@@ -0,0 +1,39 @@
+import { unstable_cache } from 'next/cache';
+import { pool } from "@/postgres";
+import type { PoolClient } from "pg";
+import type { BlogpostType } from '@/app/blog/blogpost';
+
+
+const fetchRecentPosts = unstable_cache(
+    async () => {
+        let client: PoolClient | null = null;
+        let posts: BlogpostType[] = [];
+        let errorMessage = ""; 
+
+        try {
+            client = await pool.connect();
+            const response = await client.query('SELECT * FROM posts ORDER BY created_at DESC LIMIT 10');
+            posts = response.rows;
+            posts.length === 0 && (errorMessage = "No recent posts available at the moment.");
+        } catch (error: any) {
+            console.error('Error executing query', error);
+            errorMessage = "Failed to fetch recent blogposts. Please try again later.";
+        } finally {
+            client && client.release();
+        }
+
+        return {posts: posts, error: errorMessage};
+    },
+    // cache key
+    ["recentPosts"],
+    {
+        tags: ['users'],
+        // revalidate cache every 15 min
+        revalidate: 900,
+    }
+);
+
+export default fetchRecentPosts;
+    
+
+
