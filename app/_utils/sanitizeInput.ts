80d2d86bb1658882d4c9f3f981ad92e8e6dbcb03
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-16 18:19:23 +0100

    Removed old input sanitization after reverting git commit progress

--- app/_utils/sanitizeInput.ts
+++
@@ -1,6 +0,0 @@
-import sanitize from "sanitize-html"
-
-export default function sanitizeInput(input: string[]) {
-    const cleanInput = input.map(value => sanitize(value));
-    return cleanInput;
-}
