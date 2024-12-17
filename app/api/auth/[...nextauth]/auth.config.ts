2656fbf8a2cf97a6fd5a693879e4dc8eef1863e5
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-17 14:16:50 +0100

    Replaced manually written BASE_URL for /api/auth as well SignIn page route with constants from /app/url.paths.ts

--- app/api/auth/[...nextauth]/auth.config.ts
+++ app/api/auth/[...nextauth]/auth.config.ts
@@ -1,20 +1,19 @@
 import type { User, Session, NextAuthConfig } from "next-auth";
 import type { JWT } from "next-auth/jwt";
 import { NextResponse } from "next/server";
-
-export const BASE_PATH = "/api/auth"; 
+import urlPaths from "@/app/url.paths";
 
 
 export const authConfig = {
     providers: [], // leave out for now to satisfy NextAuthConfig
-    basePath: BASE_PATH,
+    basePath: urlPaths.misc.authBasePath,
     secret: process.env.NEXTAUTH_SECRET,
     session: {
         strategy: 'jwt' as const,
         maxAge: 60 * 60 * 24 * 30, // expires in 30 days
         updateAge: 24 * 60 * 60, // 24 hours
     },
-    pages: {signIn: '/admin/sign-in'},
+    pages: {signIn: urlPaths.adminSignIn},
   
     callbacks: {
         async authorized({ auth, request }) {
@@ -23,7 +22,7 @@ export const authConfig = {
             if (PROTECTED_PATHS.includes(request.nextUrl.pathname)) {
                 if (isLoggedIn) return true;
                 else return NextResponse.redirect(new URL(
-                    `${BASE_PATH}/signin?callbackUrl=${encodeURIComponent(request.nextUrl.pathname)}`, request.url)
+                    `${urlPaths.misc.authBasePath}/signin?callbackUrl=${encodeURIComponent(request.nextUrl.pathname)}`, request.url)
                 );
             }
             return true;
