fada98f78d0af44c4000de6d431905faf8da6434
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-17 16:28:21 +0100

    Added both cached (for client-side which will receive this data from server component) and non-cached (for /admin page so I had most recent data to work with) versions of fetchPostCategories() function

---
+++ app/_utils/fetchPostCategories.ts
@@ -0,0 +1,37 @@
+import 'server-only'
+import { unstable_cache } from 'next/cache';
+import { pool } from "@/postgres";
+import type { PoolClient } from "pg";
+import type { CategoryData } from "@/app/blog/blogpost";
+
+
+// Cached version for clients
+export const fetchPostCategoriesCached = unstable_cache(
+    async () => {
+        return await fetchPostCategories();
+    },
+    ["postCategories"],
+    {
+        tags: ['recentPosts'],
+        revalidate: false, // no revalidation until another blogpost is added
+    }
+); 
+
+
+// Non-cached version for me when entering /admin page
+export async function fetchPostCategories() {
+    let client: PoolClient | null = null;
+    let categories: CategoryData[] | [] = [];
+
+    try {
+        client = await pool.connect();
+        const response = await client.query('SELECT * FROM posts_category');
+        categories = response.rows;
+    } catch (error: any) {
+        console.error('Error executing query', error);
+    } finally {
+        client && client.release();
+    }
+
+    return {categories: categories};
+}
