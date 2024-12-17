a5901173c71cfade7b85e4455170f3a6875435c0
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-17 18:19:16 +0100

    Added some color and 3d shadow effect to buttons as well as reduced size of 'styling' object where 2 classes for same element existed based on state. Now I merged 2 pairs of those classes and configured their styling to work regardless of state (so tablet+ screen width) received on mobile devices if something is clicked. Adjusted project preview name font thickness and size + moved 'group' to <Link> which now allows to change opacity and scale of project name when hovering on whole link

--- app/projects/projectPreview.tsx
+++ app/projects/projectPreview.tsx
@@ -27,18 +27,16 @@ export default function ProjectPreview({ githubUrl, localPath, imgSrc, imgAlt, t
     };
 
     const styling = {
-        link: "flex flex-col items-center w-full text-black",
-        projectImgWrapper: "w-full aspect-[4/5] rounded-xl relative group shadow-[0_0_0.15rem_grey]",
-        projectImgRegular: "w-full h-full aspect-[4/5] tablet:group-hover:opacity-40 rounded-xl",
-        projectImgTransparent: "w-full h-full aspect-[4/5] opacity-20 rounded-xl",
-        projectName: "mt-2 text-center font-medium tablet:hover:scale-105 tablet:hover:opacity-60",
+        link: "flex flex-col items-center w-full text-black group",
+        projectImgWrapper: "w-full aspect-[4/5] rounded-xl relative shadow-[0_0_0.15rem_grey]",
+        projectImg: `${buttonsVisible ? "opacity-20" : "opacity-100"} w-full h-full aspect-[4/5] tablet:opacity-100 tablet:group-hover:opacity-40 rounded-xl`,
         buttons: {
-            menu: "flex flex-col justify-center items-center gap-x-[0.8rem] absolute top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] tablet:group-hover:flex tablet:group-hover:flex-col tablet:group-hover:justify-center tablet:group-hover:items-center tablet:group-hover:gap-y-[0.8rem] tablet:group-hover:absolute tablet:group-hover:top-1/2 tablet:group-hover:left-1/2 tablet:group-hover:translate-x-[-50%] tablet:group-hover:translate-y-[-50%]",
-            menuClosed: "hidden tablet:group-hover:flex tablet:group-hover:flex-col tablet:group-hover:justify-center tablet:group-hover:items-center tablet:group-hover:gap-y-[0.8rem] tablet:group-hover:absolute tablet:group-hover:top-1/2 tablet:group-hover:left-1/2 tablet:group-hover:translate-x-[-50%] tablet:group-hover:translate-y-[-50%]",
-            button: "w-full py-2 px-4 text-nowrap transition duration-100 ease-in-out tablet:group-hover:w-full tablet:group-hover:text-nowrap tablet:group-hover:py-2 tablet:group-hover:px-4 tablet:group-hover:border-1 tablet:group-hover:border-[black] tablet:group-hover:transition tablet:group-hover:duration-100 tablet:group-hover:ease-in-out tablet:group-hover:hover:scale-105 tablet:group-hover:active:scale-105",
+            menu: `${buttonsVisible ? "flex flex-col justify-center items-center gap-y-[0.8rem] absolute top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%]" : "hidden"} tablet:hidden tablet:group-hover:flex tablet:group-hover:flex-col tablet:group-hover:justify-center tablet:group-hover:items-center tablet:group-hover:gap-y-[0.8rem] tablet:group-hover:absolute tablet:group-hover:top-1/2 tablet:group-hover:left-1/2 tablet:group-hover:translate-x-[-50%] tablet:group-hover:translate-y-[-50%]`,
+            button: "w-full py-1 px-4 bg-[rgb(235,235,235)] rounded-md text-nowrap shadow-[0_0_0.15rem_grey] transition duration-100 ease-in-out tablet:group-hover:w-full tablet:group-hover:text-nowrap tablet:group-hover:py-1 tablet:group-hover:px-4 tablet:group-hover:border-1 tablet:group-hover:border-[black] tablet:group-hover:transition tablet:group-hover:duration-100 tablet:group-hover:ease-in-out tablet:group-hover:hover:scale-105 tablet:group-hover:active:scale-105",
             toggleMenuButton: "w-6 absolute top-2 right-2 tablet:hidden",
             toggleMenuImg: "w-full h-full",
-        }
+        },
+        projectName: "mt-2 text-center font-medium text-lg tablet:group-hover:scale-105 tablet:group-hover:opacity-60",
     }
     
     return (
@@ -46,28 +44,28 @@ export default function ProjectPreview({ githubUrl, localPath, imgSrc, imgAlt, t
             
             <div className={styling.projectImgWrapper}>
                 <Image 
-                className={buttonsVisible ? styling.projectImgTransparent : styling.projectImgRegular} 
+                className={styling.projectImg} 
                 width={1000}
                 height={1000}
                 src={imgSrc} 
                 alt={imgAlt}  />
 
                 {!isFinal && 
-                    <>
-                    <div className={buttonsVisible ? styling.buttons.menu : styling.buttons.menuClosed}>
-                        <button className={styling.buttons.button} onClick={(e) => handleRedirect(e, "github")}>Github</button>
-                        <button className={styling.buttons.button} onClick={(e) => handleRedirect(e, "local")}>More info</button>
-                    </div>
+                <>
+                <div className={styling.buttons.menu}>
+                    <button className={styling.buttons.button} onClick={(e) => handleRedirect(e, "github")}>Github</button>
+                    <button className={styling.buttons.button} onClick={(e) => handleRedirect(e, "local")}>More info</button>
+                </div>
 
-                    <button className={styling.buttons.toggleMenuButton} onClick={toggleButtonMenu}>
-                        <Image 
-                        className={styling.buttons.toggleMenuImg} 
-                        width={100}
-                        height={100}
-                        src="/Icons/infoIcon.svg" 
-                        alt="show information" />
-                    </button>
-                    </>
+                <button className={styling.buttons.toggleMenuButton} onClick={toggleButtonMenu}>
+                    <Image 
+                    className={styling.buttons.toggleMenuImg} 
+                    width={100}
+                    height={100}
+                    src="/Icons/infoIcon.svg" 
+                    alt="show information" />
+                </button>
+                </>
                 }
 
             </div>
