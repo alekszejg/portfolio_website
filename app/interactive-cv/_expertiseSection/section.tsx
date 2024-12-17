dd5e560ab3bed7238ef933b26da75fd2846f6fd5
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-17 23:06:22 +0100

    Added slightly more readable structure to styling object (arguably, not done yet), removed all list loops from JS code part and placed them all inside <Subsection> component as children reactnode, mainly components consist of 'styling' object and props for SectionHeader component. I have checked, styling is currently slightly worse than I had before, however now with clearer structure I will be able to customize it quicker

--- app/interactive-cv/_expertiseSection/section.tsx
+++ app/interactive-cv/_expertiseSection/section.tsx
@@ -1,85 +1,91 @@
-import InfoSection from "@/app/interactive-cv/_reusable/infoSection";
-import Accordion from "@/app/interactive-cv/_reusable/accordion";
 import cvInfo from '@/app/interactive-cv/cv.info';
+import Subsection from "@/app/interactive-cv/_reusable/subsection";
+import SectionHeader from "@/app/interactive-cv/_reusable/sectionHeader";
+import Accordion from "@/app/interactive-cv/_reusable/accordion";
 
 
 export default function ExpertiseSection({ wrapperStyling }: {wrapperStyling: string}) {
     const styling = {
         section: wrapperStyling,
+        lists: "flex flex-col gap-y-6 mt-4 tablet:mt-6",
         approaches: {
-            wrapper: "flex flex-col items-center py-cvSectionYGap w-full px-[15%] bg-cvApproachSection group tablet:px-[8%] tablet:hover:bg-cvApproachSectionHover",
+            section: "flex flex-col items-center py-cvSectionYGap w-full px-[15%] bg-cvApproachSection group tablet:px-[8%] tablet:hover:bg-cvApproachSectionHover",
+            headerWrapper: "sticky sticky top-0 z-50 bg-cvApproachSection tablet:group-hover:bg-cvApproachSectionHover",
             headerIcon: "w-[1.7rem] select-none",
-            header: "pl-2 text-center font-medium text-xl text-nowrap",
-            list: "flex flex-col gap-y-10 mt-4 tablet:mt-6",
         },
         skills: {
-            wrapper: "flex flex-col items-center gap-y-20 w-full py-cvSectionYGap px-[15%] bg-cvSkillsSection group tablet:gap-y-16 tablet:px-[8%] tablet:box-border tablet:hover:bg-cvSkillsHover cvUltrawide:flex-row cvUltrawide:gap-x-10 cvUltrawide:items-start",
-            gearHeaderIcon: "w-10 select-none tablet:w-[2.7rem]",
-            speechHeaderIcon: "w-[1.7rem] select-none tablet:w-8 tablet:pt-[0.4rem]",
-            header: "pl-2 text-center font-medium text-xl",
-            listWrapper: "flex flex-col items-center w-full",
-            list: "flex flex-col gap-y-6 mt-4 tablet:mt-6",
+            section: "flex flex-col items-center gap-y-20 w-full py-cvSectionYGap px-[15%] bg-cvSkillsSection group tablet:gap-y-16 tablet:px-[8%] tablet:box-border tablet:hover:bg-cvSkillsHover cvUltrawide:flex-row cvUltrawide:gap-x-10 cvUltrawide:items-start",
+            contentWrapper: "flex flex-col items-center w-full",
+            techSkills: {
+                headerWrapper: "sticky sticky top-0 z-50 bg-cvSkillsSection tablet:h-[55px] tablet:group-hover:bg-cvSkillsHover",
+                headerIcon: "w-10 select-none tablet:w-[2.7rem]"
+            },
+            softSkills: {
+                headerWrapper: "tablet:h-[55px]",
+                headerIcon: "w-[1.7rem] select-none tablet:w-8 tablet:pt-[0.4rem]"
+            },
             listItem: "text-center text-clamp(1rem,5vw,1.3rem)"
         }
     };
 
-    const approachLoop = Object.keys(cvInfo.programmingApproaches).map((approach, index) => (
-        <Accordion 
-        key={`approach${index + 1}`} 
-        title={approach} 
-        description={cvInfo.programmingApproaches[approach as keyof typeof cvInfo.programmingApproaches]} />
-    ));
-
-    const approachSectionProps = {
-        wrapperStyling: styling.approaches.wrapper,
-        iconClass: styling.approaches.headerIcon,
-        headerIconSrc: "/Icons/puzzle.svg",
-        headerWrapperClass: "sticky sticky top-0 z-50 bg-cvApproachSection tablet:group-hover:bg-cvApproachSectionHover",
-        headerClass: styling.approaches.header,
-        header: "Programming approach",
-        infoListClass: styling.approaches.list,
-        infoLoop: approachLoop
-    }
-
-    const skillsLoop = (list: string[]) => {
+   
+    const genSkillsLoop = (list: string[]) => {
         return list.map((skill, index) => (
             <li key={`skill${index + 1}`} className={styling.skills.listItem}>
                 {skill}
             </li>
         ));
+    }
+
+    const approachHeaderProps = {
+        wrapperStyling: styling.approaches.headerWrapper, 
+        icon: {src: "/Icons/puzzle.svg", styling: styling.approaches.headerIcon,},
+        header: "Programming approach" 
     };
 
-    const technicalSkillsProps = {
-        wrapperStyling: styling.skills.listWrapper,
-        iconClass: styling.skills.gearHeaderIcon,
-        headerIconSrc: "/Icons/gearIcon.svg",
-        headerWrapperClass: "sticky sticky top-0 z-50 bg-cvSkillsSection tablet:h-[55px] tablet:group-hover:bg-cvSkillsHover",
-        headerClass: styling.skills.header,
-        header: "Technical Skills",
-        infoListClass: styling.skills.list,
-        infoLoop: skillsLoop(cvInfo.technicalSkills)
+    const techSkillsHeaderProps = {
+        wrapperStyling: styling.skills.techSkills.headerWrapper,
+        icon: {src: "/Icons/gearIcon.svg", styling: styling.skills.techSkills.headerIcon},
+        header: "Technical Skills"
     };
 
-    const softSkillsProps = {
-        wrapperStyling: styling.skills.listWrapper,
-        iconClass: styling.skills.speechHeaderIcon,
-        headerIconSrc: "/Icons/speechIcon.svg",
-        headerWrapperClass: "tablet:h-[55px]",
-        headerClass: styling.skills.header,
-        header: "Soft Skills",
-        infoListClass: styling.approaches.list,
-        infoLoop: skillsLoop(cvInfo.softSkills)
-    };
-
+    const softSkillsHeaderProps = {
+        wrapperStyling: styling.skills.softSkills.headerWrapper,
+        icon: {src: "/Icons/speechIcon.svg", styling: styling.skills.softSkills.headerIcon},
+        header: "Soft Skills"
+    }
+    
     return (
         <section className={styling.section}>
             
-            <InfoSection {...approachSectionProps} />
+            <Subsection wrapperStyling={styling.approaches.section}>
+                <SectionHeader {...approachHeaderProps} />
+                <ul className={styling.lists}>
+                {Object.keys(cvInfo.approaches).map((approach, index) => (
+                    <Accordion 
+                    key={`approach${index + 1}`} 
+                    title={approach} 
+                    description={cvInfo.approaches[approach as keyof typeof cvInfo.approaches]} />
+                ))};
+                </ul>
+            </Subsection>
         
-            <div className={styling.skills.wrapper}>
-                <InfoSection {...technicalSkillsProps} />
-                <InfoSection {...softSkillsProps} />
-            </div>
+            <section className={styling.skills.section}>
+                
+                <Subsection wrapperStyling={styling.skills.contentWrapper}>
+                    <SectionHeader {...techSkillsHeaderProps} />
+                    <ul>
+                        {genSkillsLoop(cvInfo.technicalSkills)};
+                    </ul>
+                </Subsection>
+                
+                <Subsection wrapperStyling={styling.skills.contentWrapper}>
+                    <SectionHeader {...softSkillsHeaderProps} />
+                    <ul className={styling.lists}>
+                        {genSkillsLoop(cvInfo.softSkills)};
+                    </ul>
+                </Subsection>
+            </section>
 
         </section> 
     );
--- app/interactive-cv/_generalSection/section.tsx
+++ app/interactive-cv/_generalSection/section.tsx
@@ -1,122 +1,97 @@
 import Image from "next/image";
-import InfoSection from "@/app/interactive-cv/_reusable/infoSection";
 import cvInfo from "@/app/interactive-cv/cv.info";
-import { Phone, Mail } from 'lucide-react';
+import Subsection from "@/app/interactive-cv/_reusable/subsection";
+import SectionHeader from "@/app/interactive-cv/_reusable/sectionHeader";
 
 
 export default function GeneralInfoSection({ wrapperStyling }: {wrapperStyling: string}) {
     const styling = {
         section: wrapperStyling,
-        stickyWrapper: "flex flex-col items-center gap-y-cvSectionYGap pt-cvSectionYGap tablet:w-[80%] tablet:mx-auto tablet:sticky tablet:top-0",
-        profile: {
-            imgWrapper: "w-[clamp(180px,55%,200px)] mx-auto aspect-square",
-            img: "w-full h-full object-cover rounded-full",
-            name: "mt-2 font-bold tracking-wider text-center text-2xl" 
+        image: {
+            wrapper: "w-[clamp(180px,55%,200px)] mx-auto aspect-square",
+            img: "w-full h-full object-cover rounded-full"
         },
-        aboutMe: {
-            wrapper: "w-full",
-            headerIcon: "w-[clamp(1rem,5vw,1.5rem)] select-none",
-            header: "pl-2 text-center font-medium text-xl",
-            text: "w-5/6 mt-4 mx-auto text-sm tablet:mt-6"
-        },
-        contact: {
-            wrapper: "w-full",
-            headerIcon: "w-[clamp(1rem,5vw,1.5rem)] select-none",
-            header: "pl-2 text-center font-medium text-xl",
-            list: "flex flex-col items-center gap-y-6 mt-4 tablet:mt-6",
-            listItem: "flex items-center w-5/6 text-left",
-            icons: "w-4 mr-2 align-[-0.2rem] select-none"
-        },
-        education: {
-            wrapper: "w-full",
-            headerIcon: "w-[clamp(1rem,5vw,1.5rem)] select-none",
-            header: "pl-2 text-center font-medium text-xl",
-            list: "flex flex-col gap-y-6 mt-4 tablet:mt-6",
-            listItem: {
-                wrapper: "flex flex-col items-center",
+        name: "mt-2 font-bold tracking-wider text-center text-2xl", 
+        stickyWrapper: {
+            wrapper: "flex flex-col items-center gap-y-cvSectionYGap pt-cvSectionYGap tablet:w-4/5 tablet:mx-auto tablet:sticky tablet:top-0",
+            contentWrappers: "w-full",
+            aboutMeText: "w-5/6 mt-4 mx-auto text-sm tablet:mt-6",
+            sectionHeaders: {
+                wrapper: "flex justify-center items-center w-full pb-2 border-b-2 border-black",
+                icons: "w-[clamp(1rem,5vw,1.5rem)] select-none"
+            },
+            lists: "flex flex-col gap-y-6 mt-4 tablet:mt-6",
+            contactListItem: "flex items-center w-5/6 text-left",
+            contactListIcons: "w-4 mr-2 align-[-0.2rem] select-none",
+            education: {
+                listItem: "flex flex-col items-center",
                 place: "w-4/5 text-left font-medium",
                 location: "w-4/5 text-left text-sm",
                 year: "text-sm font-regular-italic"
-            },
-            educationYear: "inline-block ml-[0.8rem]"
-        }
+            }
+        } 
     };
 
-    const aboutMeProps = {
-        wrapperStyling: styling.aboutMe.wrapper,
-        iconClass: styling.aboutMe.headerIcon,
-        headerIconSrc: "/Icons/contactIcon.svg",
-        headerClass: styling.aboutMe.header,
-        header: "About me",
-        descriptionClass: styling.aboutMe.text,
-        description: `A self-taught programmer with a diploma in BSc Economics, who is currently building 
-        his portfolio from various front-end and Python projects`
+    // styling is same, only iconSrc and header text differ
+    const genHeaderProps = (iconSrc: string, header: string) => {
+        const headerStyling = styling.stickyWrapper.sectionHeaders;
+        return {
+            icon: {src: iconSrc, styling: headerStyling.icons},
+            header: header
+        };
     };
-
-    const contactInfo = [
-        {icon: <Phone className={styling.contact.icons}/>, text: "alexeyguljajev@gmail.com"},
-        {icon: <Mail className={styling.contact.icons} />, text: "+49 174 6541424"}
-    ];
-
-    const contactMeLoop = contactInfo.map((info, index) => (
-        <li key={`contactInfo${index + 1}`} className={styling.contact.listItem}>
-            {info.icon}
-            {info.text}
-        </li>
-    ));
-
-    const contactSectionProps = {
-        wrapperStyling: styling.contact.wrapper,
-        headerClass: styling.contact.header,
-        header: "Contact details",
-        iconClass: styling.contact.headerIcon,
-        headerIconSrc: "/Icons/contactBookIcon.svg",
-        infoListClass: styling.contact.list,
-        infoLoop: contactMeLoop
-    };
-
-    const educationLoop = cvInfo.education.map((place, index) => {
-        return (
-            <li key={`place${index + 1}`} className={styling.education.listItem.wrapper}>
-                <p className={styling.education.listItem.place}>{place.name}</p>
-                <p className={styling.education.listItem.location}>
-                    {place.location}
-                    <span className={styling.education.listItem.year}>{place.year}</span>
-                </p>
-            </li>
-        );
-    });
-
-    const educationSectionProps = {
-        wrapperStyling: styling.education.wrapper,
-        headerClass: styling.education.header,
-        header: "Education",
-        iconClass: styling.education.headerIcon,
-        headerIconSrc: "/Icons/educationHat.svg",
-        infoListClass: styling.education.list,
-        infoLoop: educationLoop
-    };
-
+    
     return (
         <section className={styling.section}>
                 
-            <div className={styling.profile.imgWrapper}>
+            <div className={styling.image.wrapper}>
                 <Image 
-                className={styling.profile.img} 
-                width={1000} 
-                height={1000} 
+                className={styling.image.img} 
+                width={350} 
+                height={350} 
                 src="/Images/myPhoto.png" 
                 alt="My Photo" 
                 priority />
             </div>
-            <h1 className={styling.profile.name}>Alekszej Guljajev</h1>
+            <h1 className={styling.name}>Alekszej Guljajev</h1>
             
-            <div className={styling.stickyWrapper}>
-                <InfoSection {...aboutMeProps} />
-                <InfoSection {...contactSectionProps} />
-                <InfoSection {...educationSectionProps} />
+            <div className={styling.stickyWrapper.wrapper}>
+                
+                <Subsection wrapperStyling={styling.stickyWrapper.contentWrappers}>
+                    <SectionHeader {...genHeaderProps("/Icons/contactIcon.svg", "About me")} />
+                    <p className={styling.stickyWrapper.aboutMeText}>{cvInfo.aboutMe}</p>
+                </Subsection>
+                
+                <Subsection wrapperStyling={styling.stickyWrapper.contentWrappers}>
+                    <SectionHeader {...genHeaderProps("/Icons/contactBookIcon.svg", "Contact details")} />
+                    <ul className={styling.stickyWrapper.lists}>
+                        {cvInfo.contact.map((info, index) => (
+                        <li key={`contactInfo${index + 1}`} className={styling.stickyWrapper.contactListItem}>
+                            {info.icon}
+                            {info.text}
+                        </li>
+                        ))}
+                    </ul>
+                </Subsection> 
+
+                <Subsection wrapperStyling={styling.stickyWrapper.contentWrappers}>
+                    <SectionHeader {...genHeaderProps("/Icons/educationHat.svg", "Education")} />
+                    <ul className={styling.stickyWrapper.lists}>
+                        {cvInfo.education.map((place, index) => (    
+                        <li key={`place${index + 1}`} className={styling.stickyWrapper.education.listItem}>
+                            <p className={styling.stickyWrapper.education.place}>{place.name}</p>
+                            <p className={styling.stickyWrapper.education.location}>
+                                {place.location}
+                                <span className={styling.stickyWrapper.education.year}>{place.year}</span>
+                            </p>
+                        </li>
+                        ))}
+                    </ul>
+                </Subsection>
+
             </div>
             
         </section>
     );
 }
+
