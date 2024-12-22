6190cb240c3e240f69cc27fee2821a3cce8038c9
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-22 17:30:11 +0100

    Renamed overviewSection.tsx to overview.tsx for simplicity. Added logic from removed clientwrapper component

---
+++ app/projects/(project pages)/_sections/overview/overview.tsx
@@ -0,0 +1,40 @@
+"use client"
+import { useSearchParams } from "next/navigation";
+import { useLayoutEffect, useRef } from "react";
+import OverviewMedia from "@/app/projects/(project pages)/_sections/overview/media";
+import type { ImageArray } from "@/app/projects/(project pages)/_sections/overview/media";
+import Stages from "@/app/projects/(project pages)/_sections/overview/stages";
+
+
+
+type OverviewSectionProps = {
+    wrapperStyling: string,
+    images: ImageArray,
+    stages: {header: string, text: string}[],
+}
+
+
+export default function OverviewSection({ wrapperStyling, images, stages }: OverviewSectionProps) {
+    const searchParams = useSearchParams();
+    const scrollToParam = searchParams.get("scrollTo");
+    
+    const overviewSectionRef = useRef<HTMLDivElement>(null);
+
+    useLayoutEffect(() => {
+        if (scrollToParam && scrollToParam === "overview") {
+            overviewSectionRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }  
+    }, [scrollToParam]);
+
+    const styling = {
+        imageCarouselWrapper: "flex justify-between items-center",
+        stagesWrapper: "flex justify-between gap-x-6"
+    };
+
+    return (
+        <section ref={overviewSectionRef} className={wrapperStyling}>
+            <OverviewMedia images={images} wrapperStyling={styling.imageCarouselWrapper}/>
+            <Stages stages={stages} wrapperStyling={styling.stagesWrapper}/>
+        </section>
+    );
+}
