16e78bd56289e427ceb0210fcd9e844fa2e32427
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-22 14:02:21 +0100

    Added icons, interactive styling and recaptcha as temporary anti-bot spam solution. Now need to adjust logic to ask for captcha, as well as adjust dialogue's width on mobile and desktop

--- app/_homepage/basicInfo/triggerContactForm.tsx
+++ app/_homepage/basicInfo/triggerContactForm.tsx
@@ -1,20 +1,26 @@
 "use client"
 import Image from 'next/image';
-import { useState, useRef } from 'react';
-import type { FormEvent } from 'react';
-import { Mail } from 'lucide-react';
+import { useState, useRef, FormEvent } from 'react';
+import ReCAPTCHA, { ReCAPTCHAInstance } from 'react-google-recaptcha';
+import { MailPlus, UserRound, AtSign } from 'lucide-react';
 import handleMessageSubmit from '@/app/_lib/actions/handleMessageSubmit';
+
 
 export default function TriggerContactForm({ iconStyling }: {iconStyling: string}) {
     const dialogRef = useRef<HTMLDialogElement>(null);
-    const [popupVisible , setPopupVisibility] = useState(false);
+    const nameRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+    const messageRef = useRef<HTMLTextAreaElement>(null);
+    const captchaRef = useRef<ReCAPTCHAInstance | null>(null);
+    
+    const [isVisible, setVisibility] = useState(false);
    
     const toggleDialogie = () => {
-        if (popupVisible) {
-            setPopupVisibility(false);
+        if (isVisible) {
+            setVisibility(false);
             dialogRef.current?.close();
         } else {
-            setPopupVisibility(true);
+            setVisibility(true);
             dialogRef.current?.showModal()
         }
     }
@@ -22,25 +28,39 @@ export default function TriggerContactForm({ iconStyling }: {iconStyling: string
     const [ status, setStatus ] = useState({nameError: "", emailError: "", messageError: "", submitted: false}); 
     const { nameError, emailError, messageError, submitted } = status;
 
+    
+    const handleCancel = () => {
+        nameRef.current && (nameRef.current.value = '');
+        emailRef.current && (emailRef.current.value = '');
+        messageRef.current && (messageRef.current.value = '');
+        toggleDialogie()
+    };
+
+
     const handleSubmit = async (event: FormEvent) => {
         event.preventDefault();
         const { nameError, emailError, messageError, submitted } = await handleMessageSubmit(new FormData(event.currentTarget as HTMLFormElement));
         setStatus({nameError: nameError, emailError: emailError, messageError: messageError, submitted: submitted})
         if (submitted) {
-            setPopupVisibility(false);
-            setTimeout(() => {dialogRef.current?.close();}, 2000);
+            setVisibility(false);
+            setTimeout(() => {dialogRef.current?.close();}, 500);
         }
     }
 
     const styling = {
         icon: iconStyling,
-        dialogue: "w-[min(400px,50%)] pt-10 px-10 pb-16 border-1 border-black box-border rounded-2xl fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]",
+        dialogue: "px-10 py-10 box-content rounded-2xl shadow-[0_0_0.15rem_1px_grey] tablet:shadow-[0_0_0.15rem_grey]",
         form: {
-            wrapper: "flex flex-col gap-y-6",
-            input: "py-[0.6rem] px-2 rounded",
-            textarea: "h-[7rem] py-[0.6rem] px-2 rounded resize-none",
-            sendButton: "w-10 bg-transparent absolute bottom-2 right-1/2 hover:scale-105 hover:cursor-pointer",
-            sendImg: "w-full h-full select-none"
+            wrapper: "flex flex-col gap-y-6 text-base",
+            inputWrappers: "flex items-center gap-x-1.5 pl-1.5 rounded group focus-within:outline focus-within:outline-2 focus-within:outline-formInputFocused",
+            icons: "w-5 text-gray-900 opacity-40 group-focus-within:opacity-60",
+            input: "py-2 pr-2 rounded focus:outline-0",
+            textarea: "h-[7rem] py-2 px-2 rounded resize-none focus:outline focus:outline-2 focus:outline-[grey]",
+            buttons: {
+                wrapper: "flex justify-end items-center gap-x-8",
+                cancel: "w-fit hover:scale-105 hover:cursor-pointer",
+                submit: "w-fit hover:scale-105 hover:cursor-pointer",
+            }
         },
         closeButton: "w-[1.2rem] bg-transparent absolute top-2 right-2 hover:scale-105 hover:cursor-pointer",
         closeImg: "w-full h-full select-none"
@@ -48,7 +68,7 @@ export default function TriggerContactForm({ iconStyling }: {iconStyling: string
   
     return (
         <>
-        <Mail className={styling.icon} onClick={toggleDialogie} />
+        <MailPlus className={styling.icon} onClick={toggleDialogie} />
         <dialog className={styling.dialogue} ref={dialogRef}>
             <button className={styling.closeButton} onClick={toggleDialogie}>
                 <Image 
@@ -60,24 +80,30 @@ export default function TriggerContactForm({ iconStyling }: {iconStyling: string
             </button>
 
             <form className={styling.form.wrapper} onSubmit={handleSubmit}>
-                {nameError && <p>{nameError}</p>}
-                <input className={styling.form.input} type="text" name="name" placeholder="Name" />
                 
+                {nameError && <p>{nameError}</p>}
+                <div className={styling.form.inputWrappers}>
+                    <UserRound className={styling.form.icons} />
+                    <input ref={nameRef} className={styling.form.input} type="text" name="name" placeholder="Name" />
+                </div>
+
                 {emailError && <p>{emailError}</p>}
-                <input className={styling.form.input} type="text" name="contactEmail" placeholder="Email (optional)" />
+                <div className={styling.form.inputWrappers}>
+                    <AtSign className={styling.form.icons} />
+                    <input ref={emailRef} className={styling.form.input} type="text" name="contactEmail" placeholder="Email (optional)" />
+                </div>
                 
                 {messageError && <p>{messageError}</p>}
-                <textarea className={styling.form.textarea} name="message" placeholder="How can I help you?" required />
+                <textarea ref={messageRef} className={styling.form.textarea} name="message" placeholder="How can I help you?" required />
                 
-                <button className={styling.form.sendButton} type="submit">
-                    <Image 
-                    className={styling.form.sendImg}
-                    width={100}
-                    height={100}
-                    src="/Icons/sendMessage.svg" 
-                    alt="send message" />
-                </button>
-                {submitted && <p id="submittedText">Message has been submitted</p>}
+                <ReCAPTCHA ref={captchaRef} sitekey={process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY!} />
+
+                <div className={styling.form.buttons.wrapper}>
+                    <button className={styling.form.buttons.cancel} onClick={handleCancel}>CANCEL</button>
+                    <button className={styling.form.buttons.submit} type="submit">SUBMIT</button>
+                </div>
+            
+                {submitted && <p>Message has been submitted</p>}
             </form>
 
         </dialog>
