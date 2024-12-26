50a371de28e6964753f8450b41501a8eae41c50a
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-26 21:18:11 +0100

    Added mobile styling to individual project pages wrappers, they now have hidden mobile classes for tablet+ sizes and hidden tablet+ classes for mobile devices. Naming could be simplified later (avoid word section in 'styling' object, its obvious)

--- app/projects/(project pages)/styling.wrappers.ts
+++ app/projects/(project pages)/styling.wrappers.ts
@@ -1,8 +1,8 @@
 const styling = {
-    layout: "flex flex-col",
-    heroSection: "flex justify-between items-center h-[75vh] px-[5%] bg-gradient-to-135deg",
-    overviewSection: "flex gap-x-6 px-[4%] py-20 bg-blueProjectPage",
-    featureWrapper: "flex flex-col items-center py-14 px-8 tablet:flex-row tablet:justify-center tablet:gap-x-18 tablet:py-28 tablet:px-0"
+    layout: "flex flex-col max-w-[1400px] ultrawide:mx-auto",
+    heroSection: "px-[5%] py-projectYGap-mobile bg-gradient-to-135deg tablet:flex tablet:justify-around tablet:items-center tablet:gap-x-[5vw] tablet:h-[75vh]",
+    overviewSection: "flex flex-col px-[4%] py-projectYGap-mobile bg-blueProjectPage tablet:flex-row tablet:gap-x-6 tablet:py-projectYGap-pc",
+    featureWrapper: "flex flex-col items-center px-8 py-projectYGap-mobile gap-y-6 tablet:flex-row tablet:justify-center tablet:gap-x-18 tablet:gap-y-0 tablet:py-projectYGap-pc tablet:px-0"
 }
 
 export default styling;
