620b268d32fc67dd0798fcf6bc64c95a225e5ac5
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-26 21:29:22 +0100

    Adjusted JSX structure to be suitable for mobile devices. Currenly my code is running 2 loops and basically hides one of them either for mobile or tablet+ devices based on screen size. I still need to ensure its optimal way of doing it, or it would be more efficient to have event listener watching for media query match. New mobile design runs a loop where project stages come one after another without any being individually selected

--- app/projects/(project pages)/_sections/overview/stages.tsx
+++ app/projects/(project pages)/_sections/overview/stages.tsx
@@ -9,11 +9,20 @@ type StagesProps = {
 
 export default function Stages({ stages, wrapperStyling }: StagesProps) {
     const styling = {
-        progressBar: "flex flex-col justify-between items-center w-[0.2rem] h-2/3 bg-black rounded-full",
-        button: "w-5 h-5 rounded-full bg-[hsl(0,0,92%)] z-50",
-        activeButton: "w-5 h-5 rounded-full bg-red-500 z-50",
-        header: "font-medium tracking-wider",
-        text: "text-sm"
+        progressBar: "hidden tablet:flex tablet:flex-col tablet:justify-between tablet:items-center tablet:w-[0.2rem] tablet:h-2/3 tablet:bg-black tablet:rounded-full",
+        button: "tablet:w-5 tablet:h-5 tablet:rounded-full tablet:bg-[hsl(0,0,92%)] tablet:z-50",
+        activeButton: "tablet:w-5 tablet:h-5 tablet:rounded-full tablet:bg-red-500 tablet:z-50",
+        mobileStages: {
+            mainWrapper: "flex flex-col gap-y-3.5 tablet:hidden",
+            wrappers: "",
+            headers: "font-medium tracking-wider",
+            texts: "text-sm"
+        },
+        selectedStage: {
+            wrapper: "hidden tablet:block",
+            header: "font-medium tracking-wider",
+            text: "text-sm"
+        }
     };
 
     const [stageIndex, setIndex] = useState<number>(0);
@@ -36,10 +45,20 @@ export default function Stages({ stages, wrapperStyling }: StagesProps) {
                 ))}
             </div>
 
-            <div>
-                <h3 className={styling.header}>{stages[stageIndex].header}</h3>
-                <p className={styling.text}>{stages[stageIndex].text}</p>
+            <div className={styling.selectedStage.wrapper}>
+                <h3 className={styling.selectedStage.header}>{stages[stageIndex].header}</h3>
+                <p className={styling.selectedStage.text}>{stages[stageIndex].text}</p>
             </div>
+
+            <div className={styling.mobileStages.mainWrapper}>
+                {stages.map((stage, index) => (
+                    <div key={`stage${index + 1}`} className={styling.mobileStages.wrappers}>
+                        <h3 className={styling.mobileStages.headers}>{stage.header}</h3>
+                        <p className={styling.mobileStages.texts}>{stage.text}</p>
+                    </div>
+                ))}
+            </div>
+        
         </div>
     );
 }
