706c0f2927b09f54bff901a3f764c73735f5c65d
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-31 19:30:50 +0100

    Fixed horizontal gap between images and text in project feature sections

--- app/projects/(project pages)/styling.wrappers.ts
+++ app/projects/(project pages)/styling.wrappers.ts
@@ -2,7 +2,7 @@ const styling = {
     layout: "flex flex-col max-w-[1400px] ultrawide:mx-auto",
     heroSection: "px-[5%] py-projectYGap-mobile bg-gradient-to-135deg tablet:flex tablet:justify-around tablet:items-center tablet:gap-x-[5vw] tablet:h-[75vh]",
     overviewSection: "flex flex-col px-[4%] py-projectYGap-mobile bg-blueProjectPage tablet:flex-row tablet:gap-x-6 tablet:py-projectYGap-pc",
-    featureWrapper: "flex flex-col items-center px-8 py-projectYGap-mobile gap-y-6 tablet:flex-row tablet:justify-center tablet:gap-x-18 tablet:gap-y-0 tablet:py-projectYGap-pc tablet:px-0"
+    featureWrapper: "flex flex-col items-center px-8 py-projectYGap-mobile gap-y-6 tablet:flex-row tablet:justify-center tablet:gap-x-[5%] tablet:gap-y-0 tablet:py-projectYGap-pc tablet:px-0"
 }
 
 export default styling;
