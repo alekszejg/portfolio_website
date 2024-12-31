78bea98d54dfbad3d22bfb4810c5d3c93294e678
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-12-31 19:28:15 +0100

    Edited box shadow to be less noticeable, only slightly to highlight the images

--- app/projects/(project pages)/_sections/features/feature.tsx
+++ app/projects/(project pages)/_sections/features/feature.tsx
@@ -8,8 +8,8 @@ export default function Feature(props: AllFeatureProps) {
     const styling = {
         imgWrapper: `w-full aspect-[5/4] order-2 tablet:w-[clamp(375px,40%,437.50px)] ${imgSide === "right" ? "tablet:order-2" : "tablet:order-1"}`,
         svgImgWrapper: `w-[min(170px,55%)] aspect-square order-2 tablet:w-[clamp(200px,40%,300px)] ${imgSide === "right" ? "tablet:order-2" : "tablet:order-1"}`,
-        img: "w-full h-full rounded-2xl shadow-[0_0_0.55rem_black] aspect-[5/4] object-cover",
-        svgImg: "w-full h-full shadow-[0_0_0.55rem_black] tablet:rounded-2xl",
+        img: "w-full h-full rounded-2xl shadow-[0_0_0.20rem_grey] aspect-[5/4] object-cover",
+        svgImg: "w-full h-full shadow-[0_0_0.20rem_grey] tablet:rounded-2xl",
         textWrapper: `flex flex-col order-1 tablet:justify-center tablet:w-2/5 tablet:shrink-0 ${imgSide === "right" ? "tablet:order-1" : "tablet:order-2"}`,
         header: "mt-6 mb-2 text-[clamp(1.1rem,3vw,1.5rem)] tablet:mb-4",
         text: "text-[clamp(0.9rem,2vw,1.2rem)]"
