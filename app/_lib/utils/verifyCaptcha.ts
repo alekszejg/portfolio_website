eb9bcb00b8f0568748a9cb66c30edc95904b1f89
Alekszej Guljajev <alexeyguljajev@gmail.com>
2025-01-04 00:11:45 +0100

    Edited so it worked with recaptcha v3, additionally added try and catch blocks as well as mroe detailed response from this util

--- app/_lib/utils/verifyCaptcha.ts
+++ app/_lib/utils/verifyCaptcha.ts
@@ -1,15 +1,32 @@
 import urlPaths from "@/app/url.paths";
 
-export default async function verifyCaptcha(tempCaptchaValue: string): Promise<boolean> {
+type CaptchaResponse = { success: boolean; score: number };
+type Response = {success: boolean, score?: number}
+
+
+export default async function verifyCaptcha(recaptchaToken: string): Promise<Response> {
     const url = urlPaths.misc.external.verifyRecaptcha;  
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        body: `secret=${process.env.captcha_secret}&response=${tempCaptchaValue}`
-    });
 
-    const data = await response.json();
+    try {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: `secret=${process.env.captcha_secret}&response=${recaptchaToken}`
+        });
 
-    if (data.success) return true;
-    else return false;
+        if (!res.ok) return {success: false};
+        
+        const data: CaptchaResponse = await res.json();
+        
+        if (data.success && data.score > 0.5) {
+            console.log("captcha score is: ", data.score);
+            return {success: true, score: data.score};
+        } else {
+            console.log("verification failed");
+            return {success: false};
+        }
+
+    } catch {
+        return {success: false};
+    }
 }
