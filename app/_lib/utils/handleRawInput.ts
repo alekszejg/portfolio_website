a8f6c5657f9149d7ba45d60d21016cade5406958
Alekszej Guljajev <alexeyguljajev@gmail.com>
2025-01-06 23:59:15 +0100

    Removed input escape function since sanitize-html package already does that. Currently I will use 2 modes: recursiveEscape and discard (complete removal) with their default options. Later when I will need to make more custom blogposts I will tweak it to fit my needs. Also decoded escaped characters safely in user emssage output, did primitive XSS test to ensure it doesnt occur

--- app/_lib/utils/handleRawInput.ts
+++ app/_lib/utils/handleRawInput.ts
@@ -1,38 +1,24 @@
 import sanitizeHtml from "sanitize-html"
 
 
-// If one of keys is found, it's replaced by value associated with it
-const escapeCharMap: Record<string, string> = {
-    "<": "&lt;", 
-    ">": "&gt;", 
-    "&": "&amp;", 
-    '"': "&quot;", 
-    "'": "&#x27;"
-};
-
-
-// Escape symbols above with their values if found
-function escapeIllegalCharacters(inputValue: string): string {
-    const escapeKeys = Object.keys(escapeCharMap);
-    
-    return escapeKeys.reduce((acc, key) => {
-        const globalMatch = new RegExp(key, 'g');
-        return acc.replace(globalMatch, escapeCharMap[key]);
-    }, inputValue);
-}
-    
-
 // thow input away entirely or escape it, based on mode
 export default function handleRawInput(rawInput: string[], mode: "discard" | "recursiveEscape") {
+    let sanitizedValue: null | string = null;
+    
     return rawInput.map(rawValue => {
-        const sanitizedValue = sanitizeHtml(rawValue, {
-            disallowedTagsMode: mode === "discard" ? "discard" : "recursiveEscape",
-            allowedTags: mode === "discard" ? [] : false,
-            allowedAttributes: {},
-        });
-        
-        const escaptedInput = escapeIllegalCharacters(sanitizedValue);
-        return escaptedInput
+        if (mode === "discard") {
+            sanitizedValue = sanitizeHtml(rawValue, {disallowedTagsMode: "discard"});
+            return sanitizedValue; 
+        } else if (mode === "recursiveEscape") {
+            sanitizedValue = sanitizeHtml(rawValue, {disallowedTagsMode: "recursiveEscape"});
+            const actualValue = sanitizedValue
+                .replace(/&lt;/g, "<")
+                .replace(/&gt;/g, ">")
+                .replace(/&amp;/g, "&")
+                .replace(/&#x27;/g, "'")
+                .replace(/&quot;/g, '"');
+            return actualValue;
+        }    
     });
 }
 
