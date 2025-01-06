c8caaa4bc1250c952197865412a15ee6eef27448
Alekszej Guljajev <alexeyguljajev@gmail.com>
2025-01-06 17:13:42 +0100

    Made captcha badge invisible. Now have to include warning about captcha in 2 forms that I have

--- app/_layoutComponents/googleCaptchaWrapper.tsx
+++ app/_layoutComponents/googleCaptchaWrapper.tsx
@@ -1,9 +1,17 @@
 "use client"
+import { useEffect } from "react";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 
 export default function GoogleCaptchaWrapper({ children }: {children: React.ReactNode}) {
     const reCaptchaKey = process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY;
+
+    useEffect(() => {
+        const badge = document.querySelector(".grecaptcha-badge");
+        if (badge instanceof HTMLElement) {
+            badge.style.setProperty('visibility', 'hidden', 'important');
+        }
+    }, [])
 
     return (
         <GoogleReCaptchaProvider reCaptchaKey={reCaptchaKey!}>
